8b1dfd30866adfc7abd4802b279f85b1bd35e845
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-03-27 20:36:48 -0400

    3/27/2024

---
+++ src/Business/otherPackage/ArrayDemo.java
@@ -0,0 +1,16 @@
+package Business.otherPackage;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        System.out.println(args.length);
+//I can run in CM to pass some arguments
+        //System.out.println(args[3]);
+        String arg1 = "5";
+        String arg2 = "5";
+        String arg3 = "5";
+
+        int [] nums = {10,20,30,50}; //arr of integers
+        System.out.println(nums.length);
+        System.out.println(nums[1]);
+    }
+}
