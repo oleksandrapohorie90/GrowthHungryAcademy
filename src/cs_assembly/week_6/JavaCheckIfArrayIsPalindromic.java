f225b1d4396e5caa8e92f2d0df81892d603b8639
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-04-14 13:51:25 -0400

    4/14/2024

---
+++ src/cs_assembly/week_6/JavaCheckIfArrayIsPalindromic.java
@@ -0,0 +1,32 @@
+package cs_assembly.week_6;
+
+import java.util.Arrays;
+
+public class JavaCheckIfArrayIsPalindromic {
+
+    public static void main(String[] args) {
+        String[] str = {"Daaay", "DAD", "radar", "Testing", "automation"};
+        isArrayPalindromic(str);
+    }
+
+    public static void isArrayPalindromic(String[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            //to assign each string to have smth to compare to after reversing
+            String original = arr[i];
+            System.out.println("My original string is " + original);
+            //to init obj of string builder to use methods of reverse
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder = stringBuilder.append(original);
+            String reversed = stringBuilder.reverse().toString();
+
+            //to compare original and reversed options of the strings
+            //to see if they equal => palindrome
+            if (original.equals(reversed)) {
+                System.out.println("This String is palindrome - " + reversed);
+            } else {
+                System.out.println("This String is NOT palindrome - " + reversed);
+            }
+        }
+    }
+}
