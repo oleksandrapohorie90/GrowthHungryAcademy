97ea39829004df80e6bae9cefb48a419e6e1dfc7
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-04-21 16:59:44 -0400

    4/21/2024

---
+++ src/Github_200days/business/numbers/MathStuff.java
@@ -0,0 +1,23 @@
+package Github_200days.business.numbers;
+
+public class MathStuff {
+    public static void main(String[] args) {
+        System.out.println((double)7/4);
+        System.out.println((double)7/4);
+        //whichever num has a higher level of precision
+        //higher number put f to let JVM know you want a number to have decimal point
+        System.out.println(2+3*5); //multiplication is first then addition
+
+        System.out.println(4%2);
+        //modulo - %, regular division + returns the remainder
+        int x = 1;
+        //--x
+        System.out.println(x); //it is zero
+        //x++
+        System.out.println(x);//still 1
+
+        boolean myFlag = true;
+        System.out.println(!myFlag);
+
+    }
+}
