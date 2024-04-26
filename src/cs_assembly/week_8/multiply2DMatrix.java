ea1533f513f78142d7e4a652b59b054dcc366ce2
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-04-25 23:06:07 -0400

    4/25/2024

--- src/cs_assembly/week_8/multiply2DMatrix.java
+++ src/cs_assembly/week_8/multiply2DMatrix.java
@@ -6,10 +6,8 @@ public class multiply2DMatrix {
 //creating two matrices
         int a[][]={{1,1,1},{2,2,2},{3,3,3}};
         int b[][]={{1,1,1},{2,2,2},{3,3,3}};
-
 //creating another matrix to store the multiplication of two matrices
         int c[][]=new int[3][3];  //3 rows and 3 columns
-
 //multiplying and printing multiplication of 2 matrices
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
@@ -23,5 +21,4 @@ public class multiply2DMatrix {
             System.out.println();//new line
         }
     }
-
 }
