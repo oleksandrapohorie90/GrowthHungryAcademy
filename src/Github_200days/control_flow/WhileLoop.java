146ff6efa81b53a60edfa4e840d69f8a4bc08eb6
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-04-30 21:13:43 -0400

    4/30/2024

--- src/Github_200days/control_flow/WhileLoop.java
+++ src/Github_200days/control_flow/WhileLoop.java
@@ -6,15 +6,22 @@ public class WhileLoop {
     //to open terminal Alt+F12
     //to run java code in terminal -> java -cp out/production/Github_200days/control_flow WhileLoop
     public static void main(String[] args) {
-        int randomNum = new Random().nextInt(10)+1;
-        String guessedNumText = System.console().readLine("please guess a num btw 1 and 10");
-        int guessedNum = Integer.parseInt(guessedNumText);
+        int randomNum = new Random().nextInt(10) + 1;
+        String guessedNumText = null;
+        while (!"q".equals(guessedNumText)) {
+            guessedNumText = System.console().readLine("Please guess a num btw 1 and 10: ");
+            if (guessedNumText.matches("-?\\d{1,2}")) {
+                int guessedNum = Integer.parseInt(guessedNumText);
 
-        if(guessedNum==randomNum) {
-            System.out.printf("The randowm num was %d. You got it!%n", randomNum);
-        }else{
-            System.out.printf("The random number was %d.You didn't get it",randomNum);
+                if (guessedNum == randomNum) {
+                    System.out.printf("The randowm num was %d. You got it!%n", randomNum);
+                    return;
+                } else {
+                    System.out.printf("The random number was %d.You didn't get it", randomNum);
+                }
+            }
         }
-        }
+
     }
+}
 
