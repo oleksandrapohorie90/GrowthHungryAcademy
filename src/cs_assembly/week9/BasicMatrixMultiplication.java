77c82e6a76679da235329cf36bb9c3328885cbd2
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-05-02 22:52:00 -0400

    5/2/2024

--- src/cs_assembly/week9/BasicMatrixMultiplication.java
+++ src/cs_assembly/week9/BasicMatrixMultiplication.java
@@ -14,21 +14,41 @@ public class BasicMatrixMultiplication {
                 {9, 10},
                 {11, 12},
         };
-        int[][] result = matrixMultiplication(matrix1,matrix2);
+        int[][] result = matrixMultiplication(matrix1, matrix2);
+        printMatrix(result);
     }
 
-    public int[][] matrixMultiplication(int[][] a, int[][] b) {
+    public static int[][] matrixMultiplication(int[][] a, int[][] b) {
 
         if (a[0].length != b.length) {
             System.out.println("We are unable to multiply matrices");
         }
 
-    //product of above matrices will be rows of 1st X columns of 2nd
+        //product of above matrices will be rows of 1st X columns of 2nd
 
         int rowsA = a.length;
         int colB = b[0].length;
         int colA = a[0].length;
         //new matrix where we will put result
-        return
+        int[][] result = new int[rowsA][colB];
+
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colB; j++) {
+                for (int k = 0; k < colA; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    //check again
+    public static void printMatrix(int[][] matrixRes) {
+        for (int[] row : matrixRes) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
     }
 }
