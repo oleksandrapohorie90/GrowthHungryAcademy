6b93adaf825f302777e25e2be17ede69e6a3f682
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-05-05 20:36:42 -0400

    5/5/2024

---
+++ src/cs_assembly/InstantiateInnerClass.java
@@ -0,0 +1,29 @@
+package cs_assembly;
+
+public class InstantiateInnerClass {
+//    To instantiate an inner class, you must first instantiate the outer class. Then, create the inner object within the outer object with this syntax:
+
+//    OuterClass outerObject = new OuterClass();
+//    OuterClass.InnerClass innerObject = outerObject.new InnerClass();
+
+    //Constructor
+//    public Bicycle(int startCadence, int startSpeed, int startGear) {
+//        gear = startGear;
+//        cadence = startCadence;
+//        speed = startSpeed;
+//    }
+//    To create a new Bicycle object called myBike, a constructor is called by the new operator:
+//
+//    Bicycle myBike = new Bicycle(30, 0, 8);
+//new Bicycle(30, 0, 8) creates space in memory for the object and initializes its fields.
+//
+//    Although Bicycle only has one constructor, it could have others, including a no-argument constructor:
+//
+//    public Bicycle() {
+//        gear = 1;
+//        cadence = 10;
+//        speed = 0;
+//    }
+//    Bicycle yourBike = new Bicycle(); invokes the no-argument constructor to create a new Bicycle object called yourBike.
+
+}
