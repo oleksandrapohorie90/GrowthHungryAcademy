4c43cf545096e3ccf61ad6cadccf85bad7849767
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-05-05 20:39:44 -0400

    5/5/2024

--- src/cs_assembly/InstantiateInnerClass.java
+++ src/cs_assembly/InstantiateInnerClass.java
@@ -26,4 +26,20 @@ public class InstantiateInnerClass {
 //    }
 //    Bicycle yourBike = new Bicycle(); invokes the no-argument constructor to create a new Bicycle object called yourBike.
 
+    //params vs arguments
+
+//    public double computePayment(
+//            double loanAmt,
+//            double rate,
+//            double futureValue,
+//            int numPeriods) {
+//        double interest = rate / 100.0;
+//        double partial1 = Math.pow((1 + interest),
+//                - numPeriods);
+//        double denominator = (1 - partial1) / interest;
+//        double answer = (-loanAmt / denominator)
+//                - ((futureValue * partial1) / denominator);
+//        return answer;
+//    }
+//    This method has four parameters: the loan amount, the interest rate, the future value and the number of periods. The first three are double-precision floating point numbers, and the fourth is an integer. The parameters are used in the method body and at runtime will take on the values of the arguments that are passed in.
 }
