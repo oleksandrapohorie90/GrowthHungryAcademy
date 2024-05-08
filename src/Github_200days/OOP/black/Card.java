dd2ea7936fed0f75ed3c47c1e5b9993201cad9c9
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-05-07 21:42:31 -0400

    5/7/2024

--- src/Github_200days/OOP/black/Card.java
+++ src/Github_200days/OOP/black/Card.java
@@ -4,5 +4,35 @@ public class Card {
     private String suit;
     private String rank;
 
+    //initialize a newly created object
 
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public static void main(String[] args) {
+        Card card1 = new Card("Clubs", "King");
+        Card card2 = new Card("Diamonds", "");
+        //int total = card1.plus(card2);
+        //Hand hand = new Hand();
+        //myHand.add(card1);
+        //int total = myHand.getTotal();
+    }
 }
