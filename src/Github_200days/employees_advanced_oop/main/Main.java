e678926d5e57032308414542b3b43a622513ea03
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-05-09 21:58:57 -0400

    5/9/2024

--- src/Github_200days/employees_advanced_oop/main/Main.java
+++ src/Github_200days/employees_advanced_oop/main/Main.java
@@ -1,26 +1,35 @@
 package Github_200days.employees_advanced_oop.main;
 
+import java.text.NumberFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         String people = """
-            Flinstone, Fred, 1/1/1900, Programmer
-            Rubble, Barney, 2/2/1905, Manager
-            Flinstone, Wilma, 3/3/1910, Analyst
-            Rubble, Betty, 4/4/1915, CEO
-            """;
+                Flinstone, Fred, 1/1/1900, Programmer
+                Rubble, Barney, 2/2/1905, Manager
+                Flinstone, Wilma, 3/3/1910, Analyst
+                Rubble, Betty, 4/4/1915, CEO
+                """;
         String regex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}),\\s*(?<role>\\w+)\\n";
         Pattern pat = Pattern.compile(regex);
         Matcher mat = pat.matcher(people);
 
-        while(mat.find()) {
-            switch (mat.find(mat.group("role"))){
-                case "Programmer" ->
-            }
-            System.out.printf("%s,%s %s%n",mat.group("firstName"),mat.group("lastName"),);
+        int totalSalaries = 0;
+        while (mat.find()) {
+            totalSalaries +=
+                    switch (mat.group("role")) {
+                        case "Programmer" -> 3000;
+                        case "Manager" -> 3500;
+                        case "Analyst" -> 2500;
+                        case "CEO" -> 5000;
+                        default -> 0;
+                    };
         }
+        NumberFormat currencyInstance = NumberFormat.getCurrencyInstance();
+        System.out.printf("The total payout should be %s%n", currencyInstance.format(totalSalaries));
     }
-    }
+
+}
 
