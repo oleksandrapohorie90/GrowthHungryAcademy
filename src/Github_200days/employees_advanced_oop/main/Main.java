e466eef50d7daf250479a698f7e1483cef840d21
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-05-09 22:05:02 -0400

    5/9/2024

--- src/Github_200days/employees_advanced_oop/main/Main.java
+++ src/Github_200days/employees_advanced_oop/main/Main.java
@@ -8,8 +8,25 @@ public class Main {
     public static void main(String[] args) {
         String people = """
                 Flinstone, Fred, 1/1/1900, Programmer
+                Flinstone, Fred, 1/1/1900, Programmer
+                Flinstone, Fred, 1/1/1900, Programmer
+                Flinstone, Fred, 1/1/1900, Programmer
+                Flinstone, Fred, 1/1/1900, Programmer
+                Rubble, Barney, 2/2/1905, Manager
+                Rubble, Barney, 2/2/1905, Manager
+                Rubble, Barney, 2/2/1905, Manager
+                Rubble, Barney, 2/2/1905, Manager
                 Rubble, Barney, 2/2/1905, Manager
                 Flinstone, Wilma, 3/3/1910, Analyst
+                Flinstone, Wilma, 3/3/1910, Analyst
+                Flinstone, Wilma, 3/3/1910, Analyst
+                Flinstone, Wilma, 3/3/1910, Analyst
+                Flinstone, Wilma, 3/3/1910, Analyst
+                Flinstone, Wilma, 3/3/1910, Analyst
+                Rubble, Betty, 4/4/1915, CEO
+                Rubble, Betty, 4/4/1915, CEO
+                Rubble, Betty, 4/4/1915, CEO
+                Rubble, Betty, 4/4/1915, CEO
                 Rubble, Betty, 4/4/1915, CEO
                 """;
         String regex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}),\\s*(?<role>\\w+)\\n";
