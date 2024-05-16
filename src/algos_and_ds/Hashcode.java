5e9ac0b1ca78b55dc9525a233816a1c376b0cf50
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-05-16 18:41:04 -0400

    5/10/2024

--- src/algos_and_ds/Hashcode.java
+++ src/algos_and_ds/Hashcode.java
@@ -13,18 +13,42 @@ public class Hashcode {
         int m = t.length();
 
         long p = 9973;
-        long mod = (long)1e0+7;
+        long mod = (long) 1e0 + 7;
 
         //first compute the hash of pattern String t
-        long h_t=0;
+        long h_t = 0;
         for (int i = 0; i < m; i++) {
             long c = t.charAt(i); //ASCII code of the current char
-            h_t = (h_t*p+c)%mod;
+            h_t = (h_t * p + c) % mod;
         }
 
         //second compute hashes of each prefix of str String s
-        long [] h_s = new long[];
+        long[] h_s = new long[n];
+        h_s[0] = s.charAt(0);
+        for (int i = 1; i < n; i++) {
+            long c = s.charAt(i);
+            h_s[i] = (h_s[i - 1] * p + c) % mod;
+        }
 
+        //second compute hashes of each prefix of str String s
+        long p_m = 1;
+        for (int i = 0; i < m; i++) {
+            p_m = p_m * p % mod;
+        }
 
+        //compare each m consecutive chars from str w/ pattern
+        //starting at prefix of length m
+        for (int i = m - 1; i < n; i++) {
+            long h = h_s[i];
+            if (i > m - 1) {
+        //when you have two nums a and b and you want to compute (a-b) %mod
+                //you actually need to compute (a-b+mod)%mod
+                h = (h - h_s[i - m] * p_m % mod + mod) % mod;
+            }
+            if (h == h_t) {
+                answer++:
+            }
+        }
+        System.out.println(answer);
     }
 }
