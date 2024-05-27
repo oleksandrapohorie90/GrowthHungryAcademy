12875e601f805592ffe4d541f44b8a56f4d73202
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-05-27 17:53:19 -0400

    5/23/2024

---
+++ src/cs_assembly/week11/DSL.java
@@ -0,0 +1,52 @@
+package cs_assembly.week11;
+
+import java.util.Scanner;
+
+public class DSL {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String raw_expr = sc.nextLine();
+            if ("quit".equalsIgnoreCase(raw_expr)) break;
+
+            String[] expr = raw_expr.split(" ");
+            if (expr.length != 3) {
+                throw new IllegalArgumentException("It doesnt support");
+            }
+
+            try {
+                String operator = expr[0];
+                int operator1 = Integer.parseInt(expr[1]);
+                int operator2 = Integer.parseInt(expr[2]);
+
+                int result = evaluate(operator, operator1, operator2);
+                System.out.println(result);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        sc.close();
+    }
+
+
+    private static int evaluate(String operator, int operand1, int operand2) {
+
+        switch (operator) {
+            case "ADD":
+                return operand1 + operand2;
+            case "SUB":
+                return operand1 - operand2;
+            case "MUL":
+                return operand1 * operand2;
+            case "DIV":
+                if (operand2 == 0) {
+                    throw new IllegalArgumentException("Division by zero is not allowed");
+                }
+                return operand1 / operand2;
+            default:
+                throw new UnsupportedOperationException("Unsupported operation: " + operator);
+        }
+
+    }
+}
