e6a62081b9287f76aefc84de427b611d93381b2c
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-05-30 18:37:41 -0400

    5/30/2024

--- src/algos_and_ds/week10/HashMapTwoSum.java
+++ src/algos_and_ds/week10/HashMapTwoSum.java
@@ -1,5 +1,8 @@
 package algos_and_ds.week10;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapTwoSum {
     /**
      * nums 2,7,11,15
@@ -17,6 +20,25 @@ public class HashMapTwoSum {
      *         j=0,j<i,j++
      *     }
      * }
+     * Is there a num to the left before numsi => target - numsi = numsj
+     * (key=numsj, value=j)
      */
+    public int[]twoSum(int[ ]nums, int target){
+
+
+        Map<Integer, Integer> HT = new HashMap<>();
+
+        //Time: O(N)
+        //Space: O(N)
+        for (int i = 0; i < nums.length; i++) { //O(N)
+            int complement = target-nums[i]; //nums[j] == target - nums[i], j<1
+            if(HT.containsKey(complement)){ //O(1)
+                return new int[] {i,HT.get(complement)};//O(1)
+            }
+            HT.put(nums[i],i); //insert key-value pair (k=nums[i], v=i)  //O(1)
+        }
+        //in case there is no solution
+        return null;
+    }
 
 }
