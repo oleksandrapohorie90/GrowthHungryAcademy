dc3033b636e3560311e4561587b1f9e8dcc71413
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-05-30 18:26:30 -0400

    5/30/2024

--- src/algos_and_ds/week10/HashMapTwoSum.java
+++ src/algos_and_ds/week10/HashMapTwoSum.java
@@ -5,5 +5,18 @@ public class HashMapTwoSum {
      * nums 2,7,11,15
      * target=9
      * answer=[0,1]
+     * Brute Force solution-simplest way
+     * O(n)^2 - time complexity
+     * Big theta (1) - space complexity
+     * for i=1...n-1{
+     *     for j=0...i-1{
+     *         if numsi+numsj==target
+     *         return [i,j]
+     *
+     *         i=1..n-1
+     *         j=0,j<i,j++
+     *     }
+     * }
      */
+
 }
