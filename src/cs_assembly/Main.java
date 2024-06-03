2d15128d3698ba47284b66ae023407e4f1e1f140
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-06-03 18:16:52 -0400

    6/3/2024

--- src/cs_assembly/Main.java
+++ src/cs_assembly/Main.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        //use java to create lexer that will parse to token types
         ArrayList<Lexer.Token> tokens = new ArrayList<>();
 
         tokens.add(new Lexer.Token(Lexer.TokenType.CONFIG, "Config"));
         tokens.add(new Lexer.Token(Lexer.TokenType.STRING, "num_users"));
         tokens.add(new Lexer.Token(Lexer.TokenType.NUMBER, "100"));
-
 
         for(Lexer.Token token : tokens){
             System.out.println(token);
