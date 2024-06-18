90e8b8ddb1987b9a8fa06284e514a098ebb21afb
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-06-17 22:44:45 -0400

    6/17/2024

--- src/cs_assembly/CS_Interpreters_Parsers/Lexer.java
+++ src/cs_assembly/CS_Interpreters_Parsers/Lexer.java
@@ -1,21 +1,23 @@
 package cs_assembly.CS_Interpreters_Parsers;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Lexer {
 
-    public static List<Token> getHardcodedTokens(){
+    public static List<Token> getHardcodedTokens() {
         return Arrays.asList(
-                new Token (Token.Type.NUMBER, "3"),
-        new Token (Token.Type.PLUS, "+"),
-        new Token (Token.Type.NUMBER, "5"),
-        new Token (Token.Type.MULTIPLY, "*"),
-        new Token (Token.Type.LPAREN, "("),
-        new Token (Token.Type.NUMBER, "10"),
-        new Token (Token.Type.MINUS, "-"),
-        new Token (Token.Type.NUMBER, "4"),
-        new Token (Token.Type.RPAREN, ")")
+                new Token(Token.Type.NUMBER, "3"),
+                //new Token(Token.Type.PLUS, "+"),
+                new Token(Token.Type.MULTIPLY, "*"),
+                new Token(Token.Type.NUMBER, "5"),
+                new Token(Token.Type.MULTIPLY, "*"),
+                //new Token(Token.Type.LPAREN, "("),
+                new Token(Token.Type.NUMBER, "10"),
+                //new Token(Token.Type.MINUS, "-"),
+                new Token(Token.Type.MULTIPLY, "*"),
+                new Token(Token.Type.NUMBER, "4")
+                //new Token(Token.Type.RPAREN, ")")
+        );
     }
 }
--- src/cs_assembly/CS_Interpreters_Parsers/Parser.java
+++ src/cs_assembly/CS_Interpreters_Parsers/Parser.java
@@ -1,103 +1,105 @@
-//package cs_assembly.week13;
-//
-//import java.lang.reflect.Type;
-//import java.util.List;
-//
-//public class Parser {
-//    private final List<Tokens> tokens;
-//    private int currentPos; //num of tokens
-//    private Tokens currentToken;
-//
-//    public Parser(List<Tokens> tokens) {
-//        this.tokens = tokens;
-//        currentPos = 0;
-//        currentToken = tokens.get(currentPos);
-//    }
-//    private ASTNode expression() {
-//
-//        ASTNode node = term();
-//        while (currentToken != null && (currentToken.type == Type.PLUS || currentToken.type == Type.MINUS)) {
-//            Tokens token = currentToken;
-//            consume(currentToken.type);
-//            node = new BinaryOpNode(node, term(), token);
-//        }
-//    }
-//    public ASTNode parse() {
-//        return term();
-//    }
-//
-//    private ASTNode term() {
-//
-//        ASTNode node = factor();
-//        while (currentToken != null && (currentToken.type == Type.MULTIPLY || currentToken.type == Type.DIVIDE)) {
-//            Tokens token = currentToken;
-//            consume(currentToken.type);
-//            node = new BinaryOpNode(node, factor(), token);
-//        }
-//    }
-//
-//    private void consume(Type type) {
-//        if (currentToken.type == type) {
-//            currentPos++;
-//            if (currentPos < tokens.size()) {
-//                currentToken = tokens.get(currentPos);
-//            } else {
-//                currentToken = null;
-//
-//            }
-//        } else {
-//            throw new Parserexception("unexpected token " + type);
-//        }
-//    }
-//
-//    private ASTNode factor() {
-//        //factor is just an expression or a number
-//        //it has to be a number token and we just return it
-//        //we are pointing into a token
-//
-//        //now it could be a paranthesis or a number
-//        Tokens token = currentToken;
-//        if(token.type==Tokens.type.NUMBER) {
-//            consume(Type.NUMBER);
-//            return new NumberNode(token);
-//        }
-//
-//        if (token.type == Type.LPAREN){
-//            consume(Tokens.type.LPAREN);
-//        }
-//        throw  new Parserexception("unexpected token found : "+token);
-//
-//    }
-//
-//    abstract class ASTNode {
-//
-//    }
-//
-//    class Number extends ASTNode {
-//
-//        Tokens numberToken;
-//        private final int value;
-//
-//        public Number(Tokens numberToken) {
-//            this.numberToken = numberToken;
-//            this.value = Integer.parseInt(value);
-//        }
-//    }
-//
-//    class BinaryOp extends ASTNode {
-//
-//        //left node
-//        ASTNode left;
-//        //right node
-//        ASTNode right;
-//        Tokens operationToken;
-//
-//        public BinaryOp(ASTNode left, ASTNode right, Tokens operationToken) {
-//            this.left = left;
-//            this.right = right;
-//            this.operationToken = operationToken;
-//        }
-//    }
-//
-//
-//}
+package cs_assembly.week13;
+
+import cs_assembly.CS_Interpreters_Parsers.Token;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class Parser {
+    private final List<Token> tokens;
+    private int currentPos; //num of tokens
+    private Tokens currentToken;
+
+    public Parser(List<Tokens> tokens) {
+        this.tokens = tokens;
+        currentPos = 0;
+        currentToken = tokens.get(currentPos);
+    }
+    private ASTNode expression() {
+
+        ASTNode node = term();
+        while (currentToken != null && (currentToken.type == Type.PLUS || currentToken.type == Type.MINUS)) {
+            Tokens token = currentToken;
+            consume(currentToken.type);
+            node = new BinaryOpNode(node, term(), token);
+        }
+    }
+    public ASTNode parse() {
+        return term();
+    }
+
+    private ASTNode term() {
+
+        ASTNode node = factor();
+        while (currentToken != null && (currentToken.type == Type.MULTIPLY || currentToken.type == Type.DIVIDE)) {
+            Tokens token = currentToken;
+            consume(currentToken.type);
+            node = new BinaryOpNode(node, factor(), token);
+        }
+    }
+
+    private void consume(Type type) {
+        if (currentToken.type == type) {
+            currentPos++;
+            if (currentPos < tokens.size()) {
+                currentToken = tokens.get(currentPos);
+            } else {
+                currentToken = null;
+
+            }
+        } else {
+            throw new Parserexception("unexpected token " + type);
+        }
+    }
+
+    private ASTNode factor() {
+        //factor is just an expression or a number
+        //it has to be a number token and we just return it
+        //we are pointing into a token
+
+        //now it could be a paranthesis or a number
+        Tokens token = currentToken;
+        if(token.type==Tokens.type.NUMBER) {
+            consume(Type.NUMBER);
+            return new NumberNode(token);
+        }
+
+        if (token.type == Type.LPAREN){
+            consume(Tokens.type.LPAREN);
+        }
+        throw  new Parserexception("unexpected token found : "+token);
+
+    }
+
+    abstract class ASTNode {
+
+    }
+
+    class Number extends ASTNode {
+
+        Tokens numberToken;
+        private final int value;
+
+        public Number(Tokens numberToken) {
+            //all nodes will either be binary operation or a number, each of them is left and right node
+            this.numberToken = numberToken;
+            this.value = Integer.parseInt(value);
+        }
+    }
+
+    class BinaryOp extends ASTNode {
+        //left node
+        ASTNode left;
+        //right node
+        ASTNode right;
+        Token operationToken;
+
+        public BinaryOp(ASTNode left, ASTNode right, Token operationToken) {
+            this.left = left;
+            this.right = right;
+            this.operationToken = operationToken;
+        }
+    }
+
+
+}
