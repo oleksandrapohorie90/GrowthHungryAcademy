73c8eb70ecc7aa08a3e48209c6d90488277cc02a
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-06-22 07:15:07 -0400

    6/22/2024

---
+++ src/algos_and_ds/DS_QUEUE/DesignFrontMiddleBackQueue.java
@@ -0,0 +1,56 @@
+package algos_and_ds.DS_QUEUE;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DesignFrontMiddleBackQueue {
+
+        Deque<Integer> dq1, dq2;
+        public DesignFrontMiddleBackQueue() {
+            dq1 = new ArrayDeque<Integer>();
+            dq2 = new ArrayDeque<Integer>();
+        }
+
+        public void pushFront(int val) {
+            dq1.addFirst(val);
+        }
+
+        public void pushBack(int val) {
+            dq2.addLast(val);
+        }
+
+        public void pushMiddle(int val) {
+            while(dq1.size() + 1 < dq2.size())
+                dq1.addLast(dq2.removeFirst());
+            while(dq1.size() > dq2.size())
+                dq2.addFirst(dq1.removeLast());
+            dq1.addLast(val);
+        }
+
+        public int popFront() {
+            if(!dq1.isEmpty())
+                return dq1.removeFirst();
+            if(!dq2.isEmpty())
+                return dq2.removeFirst();
+            return -1;
+        }
+
+        public int popMiddle() {
+            if(dq1.isEmpty() && dq2.isEmpty())
+                return -1;
+            while(dq1.size() < dq2.size())
+                dq1.addLast(dq2.removeFirst());
+            while(dq1.size() > dq2.size() + 1)
+                dq2.addFirst(dq1.removeLast());
+            return !dq1.isEmpty() ? dq1.removeLast() : dq2.removeFirst();
+        }
+
+        public int popBack() {
+            if(!dq2.isEmpty())
+                return dq2.removeLast();
+            if(!dq1.isEmpty())
+                return dq1.removeLast();
+            return -1;
+        }
+    }
+
