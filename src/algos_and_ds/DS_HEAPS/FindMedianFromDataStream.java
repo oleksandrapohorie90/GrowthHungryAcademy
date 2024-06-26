5913046b34a91cf8a73d81ea19c8c5243a6b7d47
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-06-25 22:09:24 -0500

    6/25/2024

---
+++ src/algos_and_ds/DS_HEAPS/FindMedianFromDataStream.java
@@ -0,0 +1,22 @@
+package algos_and_ds.DS_HEAPS;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class FindMedianFromDataStream {
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+    public FindMedianFromDataStream() {
+    }
+    public void addNum(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+        if (minHeap.size() > maxHeap.size())
+            maxHeap.offer(minHeap.poll());
+    }
+    public double findMedian() {
+        if (maxHeap.size() > minHeap.size()) return maxHeap.peek();
+        return (minHeap.peek() + maxHeap.peek()) / 2.0d;
+    }
+}
