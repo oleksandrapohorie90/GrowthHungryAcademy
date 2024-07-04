18ec7f6e83bb55d1bc007b85187dc33d494afd9a
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-07-04 14:45:17 -0400

    7/4/2024

--- src/cs_and_programming/CS_Interpreters_ASTS/SemanticAnalyzer.java
+++ src/cs_and_programming/CS_Interpreters_ASTS/SemanticAnalyzer.java
@@ -8,58 +8,59 @@ import java.util.Set;
 import java.util.Stack;
 
 public class SemanticAnalyzer {
-        private final Stack<Set<String>> scopes = new Stack<>();
+    private final Stack<Set<String>> scopes = new Stack<>();
 
-        //parser returns the root node
-        //go thru all the children and check if everything is good
-        public void visit(ASTNode node) throws ParserException {
-            //if smth is wrong we should throw an exception to know whats wrong
-            //go thru each node
+    //parser returns the root node
+    //go thru all the children and check if everything is good
+    public void visit(ASTNode node) throws ParserException {
+        //if smth is wrong we should throw an exception to know whats wrong
+        //go thru each node
 
-            if(node instanceof BinaryOpNode){
-                visit(((BinaryOpNode)node).left);
-                visit(((BinaryOpNode)node).right);
+        if (node instanceof BinaryOpNode) {
+            visit(((BinaryOpNode) node).left);
+            visit(((BinaryOpNode) node).right);
 
-            }else if (node instanceof NumberNode){
-                //nothing, semantic analyzer doesn't return anything
-                //unless there is an error
+        } else if (node instanceof NumberNode) {
+            //nothing, semantic analyzer doesn't return anything
+            //unless there is an error
 
-            }else if (node instanceof Vardecl varDecl){
-                String varName = varDecl.varNode.name;
-                if(!isvariableDefined(varName)){
-                    throw new ParserException("Identifier already defined: " + varName);
+        } else if (node instanceof Vardecl varDecl) {
+            String varName = varDecl.varNode.name;
+            if (!isvariableDefined(varName)) {
+                throw new ParserException("Identifier already defined: " + varName);
 
-                }
-                //put varName to current scope
+            }
+            //put varName to current scope
+            scopes.peek().add(varName);//take the latest element, declare var in scope
 
-            }else if (node instanceof Var){
-                String varName= ((Var)node).name;
-                if(!isvariableDefined(varName)){
-                    throw new ParserException("Unexpected identifier: " + varName);
+        } else if (node instanceof Var) {
+            String varName = ((Var) node).name;
+            if (!isvariableDefined(varName)) {
+                throw new ParserException("Unexpected identifier: " + varName);
 
-                }
-
-            }else if (node instanceof Assign assignNode){
-                String varName = assignNode.left.name;
-                if(!isvariableDefined(varName)){
-                    throw new ParserException("Unexpected identifier: " + varName);
-
-                }
-                visit(assignNode.right);
-
-            }else if (node instanceof Block block){
-                scopes.push(new HashSet<>());
-                //enter scope
-                for(ASTNode statement: block.statements){
-                    visit(statement);
-                }
-                //exit scope
-                scopes.pop();
-            } else {
-                throw new ParserException("Unexpected AST Node: " + node.getClass().getCanonicalName());
             }
 
+        } else if (node instanceof Assign assignNode) {
+            String varName = assignNode.left.name;
+            if (!isvariableDefined(varName)) {
+                throw new ParserException("Unexpected identifier: " + varName);
+
+            }
+            visit(assignNode.right);
+
+        } else if (node instanceof Block block) {
+            scopes.push(new HashSet<>());//enter a new scope
+            //enter scope
+            for (ASTNode statement : block.statements) {
+                visit(statement);
+            }
+            //exit scope
+            scopes.pop();
+        } else {
+            throw new ParserException("Unexpected AST Node: " + node.getClass().getCanonicalName());
         }
+
     }
+}
 
 
