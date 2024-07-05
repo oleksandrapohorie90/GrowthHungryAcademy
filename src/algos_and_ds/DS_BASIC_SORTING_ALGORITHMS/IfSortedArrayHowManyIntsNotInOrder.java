403986820128fd446804ed91b8b8977380de67cb
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-07-05 14:29:58 -0400

    7/5/2024

---
+++ src/algos_and_ds/DS_BASIC_SORTING_ALGORITHMS/IfSortedArrayHowManyIntsNotInOrder.java
@@ -0,0 +1,17 @@
+package algos_and_ds.DS_BASIC_SORTING_ALGORITHMS;
+
+public class IfSortedArrayHowManyIntsNotInOrder {
+
+    public int heightChecker(int[] heights){
+        //Bubble sort
+        //Iterate over the array(possibly multiple times, up to [n-1] times)
+        //if a[i] > a[i+1]
+        //swap(a[i],a[i+1])
+        //swapped=true
+        //after iterating over the array, check swapped
+        //if swapped is true
+            //iterate over the array again
+        //else
+        //stop the algorithm, array is sorted
+    }
+}
