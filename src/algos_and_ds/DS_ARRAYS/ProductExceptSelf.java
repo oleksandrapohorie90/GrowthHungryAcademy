d5865c52b77fe92caee345b13eb5c7eb0395e247
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-07-10 22:10:01 -0400

    7/10/2024

--- src/algos_and_ds/DS_ARRAYS/ProductExceptSelf.java
+++ src/algos_and_ds/DS_ARRAYS/ProductExceptSelf.java
@@ -16,6 +16,11 @@ public class ProductExceptSelf {
         nums[0...2]=[1,2,3]     pref_mul[2] = 1*2*3 = 6 = pref_mul[1] * nums[2]
         nums[0...3]=[1,2,3,4]   pref_mul[3] = 1*2*3*4 = 24 = pref_mul[2] * nums[3]
 
+        nums[] = [4]
+        nums[2..3] = [3,4]
+        nums[1..3] = [2,3,4]
+        nums[0..3] = [1,2,3,4]
+
 
         int n = nums.length;
         int[] pref_mul = new int[n];
@@ -26,6 +31,14 @@ public class ProductExceptSelf {
         for i=1..n-1
         pref_mul[i]=pref_mul[i-1] * nums[i]
         suff_mul[n-1]=nums[n-1] -> will equal to 4
+        for i=n-2 0
+        suff_mul[i]=nums[i]*suff_mul[i+1]
+        result = [1...1]
+                    n
+        result[0] = suff_mul[1] -> nums[1..n-1]
+        result[n-1] = pref_mul[n-2] -> nums[0..n-2]
+        indices  i=1..n-2, result[i] -? pref_mul[i-1] * suff_mul[i+1]
+                                               0..i-1        i+1..n-1
          */
     }
 }
