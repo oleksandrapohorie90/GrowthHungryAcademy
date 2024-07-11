9bbb9cc13a582fd3347e99da3ee932153eb7b623
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-07-11 14:02:56 -0400

    7/10/2024

--- src/algos_and_ds/DS_ARRAYS/FindMaxLength.java
+++ src/algos_and_ds/DS_ARRAYS/FindMaxLength.java
@@ -5,10 +5,10 @@ public class FindMaxLength {
     public int findMaxLength(int [] nums){
         int n = nums.length;
     int answer = 0;
-        for (int start = 0; start < n; start++) {
+        for (int start = 0; start < n; start++) { //O(n)
             int zeroes = 0;
             int ones = 0;
-            for (int i = start; i < n; i++) {
+            for (int i = start; i < n; i++) { //O(n)
                 if(nums[i]==0) zeroes++;
                 else ones++;
 
