9fa3be436df7020359a08878b1493d7f5e4b9839
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-07-11 22:50:32 -0400

    7/11/2024

--- src/algos_and_ds/DS_ARRAYS/ContiguousArray.java
+++ src/algos_and_ds/DS_ARRAYS/ContiguousArray.java
@@ -4,22 +4,30 @@ import java.util.HashMap;
 
 public class ContiguousArray {
 
-    public int findMaxlength(int [] nums){
+    public int findMaxlength(int[] nums) {
         int n = nums.length;
-        int [] pref = new int[n];
+        int[] pref = new int[n];
 
-        pref[0] = nums[0]==0?-1:+1;
+        pref[0] = nums[0] == 0 ? -1 : +1;
         for (int i = 0; i < n; i++) {
-            pref[i]= pref[i-1];
-            if(nums[i]==0){
+            pref[i] = pref[i - 1];
+            if (nums[i] == 0) {
                 pref[i]--;
 
-            }else {
+            } else {
                 pref[i]++;
             }
         }
-        HashMap<Integer,Integer>hm= new HashMap<>();
-        hm.put(0,-1);
-        int answer=0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0, -1);
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            if (hm.containsKey(pref[i])) {
+                answer = Math.max(answer, i - hm.get(pref[i]));
+            } else {
+                hm.put(pref[i], i);
+            }
+        }
+        return answer;
     }
 }
