d8dafa344e390ab1e1130cf1968550d65a4f8171
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-08-01 21:09:26 -0400

    8/1/2024

---
+++ src/data_structures_and_algorithms/week17/BinarySearch.java
@@ -0,0 +1,13 @@
+package data_structures_and_algorithms.week17;
+
+public class BinarySearch {
+    public int binarySearch(int[] nums){
+        /**
+         * Assume we have an array nums of length n(nums[0...n-1]) sorted
+         * in ascending order
+         * Lets find an int x in nums and return its index, or return -1 if its
+         * not present
+         */
+        int left
+    }
+}
