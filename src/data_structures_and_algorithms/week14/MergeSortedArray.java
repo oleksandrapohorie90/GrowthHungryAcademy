1f1f9610a06cc7157c610e065535556876713411
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-08-12 08:21:43 -0400

    8/12/2024

--- src/data_structures_and_algorithms/week14/MergeSortedArray.java
+++ src/data_structures_and_algorithms/week14/MergeSortedArray.java
@@ -3,9 +3,13 @@ package data_structures_and_algorithms.week14;
 public class MergeSortedArray {
     //Idea: we maintain 3 pointers
     //1st - at the end of numbers here, starts with 3 in this case
-    //nums1 = [1, 2, 3, 0, 0, 0], m = 3, nums2 = [2, 5, 6], m = 3(i=m-1)
+    //nums1 = [1, 2, 3, 0, 0, 0], m = 3, nums2 = [2, 5, 6], n = 3(i=n-1)
     //2nd pointer nums2 = [2, 5, 6], n = 3
-    //we start with the largest numbers from both arrays and the 3rd pointer is where we are going to write the next number
+    //3rd pointer at the end of nums1 = [1, 2, 3, 0, 0, 0] -> which is 0
+    //we start with the largest numbers from both arrays and the 3rd pointer is where we are going to write the next number, so start with 2 lasrgest nums and we already know we will put 3rd pointer at the very last nums1
+    //we compare 3 and 6 and 6 is larger, so we put 6 at the 3rd pointer position -> [1, 2, 3, 0, 0, 6] , next decrement 3rd pointer(k) to point to the next position
+    //we decrement the 2nd pointer since we wrote 6 already
+    //next 3 and 5, 5 is larger -> [1, 2, 3, 0, 5, 6]
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1;
