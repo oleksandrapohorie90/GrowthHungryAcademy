c329b7db7d802fd43a1da8c6c3679d1cb2eeb80f
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-08-28 07:17:33 -0400

    8/28/2024

--- src/data_structures_and_algorithms/week16/TwoSums.java
+++ src/data_structures_and_algorithms/week16/TwoSums.java
@@ -6,6 +6,43 @@ import java.util.Map;
 
 public class TwoSums {
     //will have to use HashMap
+
+    /**
+     *
+     * public int[] twoSum(int[] nums, int target) {
+     *         //          i=5
+     *         // [..., nums[i] = 17, ...]
+     *         // target = 77
+     *         // lastSeenSoFar = { 22: 0, 61: 1, 23: 4, 56: 3, 12: 2 }
+     *         // lastSeenSoFar.containsKey(77 - 17, i.e. 60) -> false
+     *         // No problem, let's record -> lastSeenSoFar.put(17, 5) // key=17 (number), value=5 (lastSeenIndex)
+     *         // lastSeenSoFar = { 22: 0, 61: 1, 23: 4, 56: 3, 12: 2, 17: 5}
+     *
+     *         //          i=6
+     *         // [..., nums[i] = 21, ...]
+     *         // target = 77
+     *         // lastSeenSoFar = { 22: 0, 61: 1, 23: 4, **56: 3**, 12: 2, 17: 5}
+     *         // lastSeenSoFar.containsKey(77 - 21, i.e. 56) -> true
+     *         // Cool! Return {lastSeenSoFar.get(56) [i.e. 3], i [i.e. 6]}
+     *
+     *         // Map<Number, IndexLastSeen>
+     *         Map<Integer, Integer> lastSeenSoFar = new HashMap<>();
+     *         for (int i = 0; i < nums.length; ++i) {
+     *             int b = nums[i];
+     *             int a = target - b;
+     *             if (lastSeenSoFar.containsKey(a)) {
+     *                 // Found pair of (a, b), so that a + b = target
+     *                 // Number a last seen at index lastSeenSoFar.get(a)
+     *                 // and number b is last seen at index i.
+     *                 return new int[]{lastSeenSoFar.get(a), i};
+     *             }
+     *             // Save that number b (nums[i]) last seen at index i.
+     *             lastSeenSoFar.put(b, i);
+     *         }
+     *         return new int[]{};
+     *     }
+     * }
+     */
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 6};
         int target = 4;
@@ -13,32 +50,18 @@ public class TwoSums {
     }
 
     public static int[] twoSum(int[] nums, int target) {
-        //          i=5
-        // [..., nums[i] = 17, ...]
-        // target = 77
-        // lastSeenSoFar = { 22: 0, 61: 1, 23: 4, 56: 3, 12: 2 }
-        // lastSeenSoFar.containsKey(77 - 17, i.e. 60) -> false
-        // No problem, let's record -> lastSeenSoFar.put(17, 5) // key=17 (number), value=5 (lastSeenIndex)
-        // lastSeenSoFar = { 22: 0, 61: 1, 23: 4, 56: 3, 12: 2, 17: 5}
 
-        //          i=6
-        // [..., nums[i] = 21, ...]
-        // target = 77
-        // lastSeenSoFar = { 22: 0, 61: 1, 23: 4, **56: 3**, 12: 2, 17: 5}
-        // lastSeenSoFar.containsKey(77 - 21, i.e. 56) -> true
-        // Cool! Return {lastSeenSoFar.get(56) [i.e. 3], i [i.e. 6]}
-
-        // Map<Number, IndexLastSeen>
-        Map<Integer, Integer> lastSeenSoFar = new HashMap<>();
+    Map<Integer, Integer> lasSeenSoFar = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int a = nums[i];
             int b = target - a;
-            if (lastSeenSoFar.containsKey(b)) {
-                return new int[]{lastSeenSoFar.get(b), i};
+            if(lasSeenSoFar.containsKey(b)){
+                return new int[]{lasSeenSoFar.get(b), i};
             }
-            lastSeenSoFar.put(a, i);
+            lasSeenSoFar.put(a,i);
         }
-        return new int[]{};
-    }
+        return new int []{};
+        }
+
 
 }
