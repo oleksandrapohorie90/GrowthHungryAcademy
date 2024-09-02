ceb361e534df95f6612f2af103d8e55a2d080b84
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-09-02 13:24:59 -0400

    9/1/2024

--- src/data_structures_and_algorithms/week19/TowersOfHanoi.java
+++ src/data_structures_and_algorithms/week19/TowersOfHanoi.java
@@ -10,7 +10,7 @@ public class TowersOfHanoi {
         if (n == 0) return;
         int aux = (1 + 2 + 3) - (from + to);
         //to know the aux rot, if from and to are subtr then we know which one is the aux
-        //(1) - I want to ove n-1 disks from the starting rode to the auxilary(helper)rode
+        //(1) - I want to move n-1 disks from the starting rode to the auxilary(helper)rode
         hanoi(n - 1, from, aux);
 
         //(2) next I move largest 'n'th disk from the starting rod to the finish rod
@@ -19,7 +19,7 @@ public class TowersOfHanoi {
         //(3) then I have 'n-1' disks on the auxilary rode, so I move those disks from the aux(helper) to the finish rode
         hanoi(n - 1, aux, to);
     }
-
+//FIRST STEP
     public void run(InputStreamReader in, PrintWriter out) {
         //int n = in.nextInt();
         //hanoi(n, 1, 3); -> in the very I move n disks from the 1 rode to the 3rd rode
