4151dfa0b9c69f8cfded7bde00b5d6ba78830ed1
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-09-05 08:11:28 -0400

    9/3/2024

--- src/cs_and_programming/CS_DESIGN_PATTERNS/Main.java
+++ src/cs_and_programming/CS_DESIGN_PATTERNS/Main.java
@@ -1,5 +1,6 @@
 package cs_and_programming.CS_DESIGN_PATTERNS;
 
+import static cs_and_programming.CS_DESIGN_PATTERNS.BuilderDesignPattern.EvictionStrategy.FIFO;
 import static cs_and_programming.CS_DESIGN_PATTERNS.BuilderDesignPattern.EvictionStrategy.LRU;
 
 public class Main {
@@ -17,11 +18,22 @@ public class Main {
         System.out.println(cache.containsKey("Alex")); //false, since capacity was reached and Alex got removed
 
         System.out.println(cache.size()); //always will be 5
-        SingletonDesignPattern cache1 = SingletonDesignPattern.getInstance(100,LRU); // Creates the instance
-        SingletonDesignPattern cache2 = SingletonDesignPattern.getInstance(200,LRU); // Returns the existing instance
+        SingletonDesignPattern cache1 = SingletonDesignPattern.getInstance(100,FIFO); // Creates the instance
+        SingletonDesignPattern cache2 = SingletonDesignPattern.getInstance(200,FIFO); // Returns the existing instance
         System.out.println(cache1 == cache2); // true - both references point to the same instance
 
-        FactoryDesignPattern cacheFactory = new LRUCacheFactory();
-        SingletonDesignPattern lruCache = cacheFactory.createCache(100, LRU);
+        //create cache using LRU strategy
+        FactoryDesignPattern lruFactory = new LRUCacheFactory();
+        SingletonDesignPattern lruCache = lruFactory.createCache(5, LRU);
+
+        lruCache.put("Alla", 15);
+        System.out.println(lruCache.get("Alla")); // Should print 15
+
+        //create cache using FIFO strategy
+        FactoryDesignPattern fifoFactory = new FIFOCacheFactory();
+        SingletonDesignPattern fifoCache = fifoFactory.createCache(5, FIFO);
+
+        fifoCache.put("Sasha", 20);
+        System.out.println(fifoCache.get("Sasha")); // Should print 20
     }
 }
