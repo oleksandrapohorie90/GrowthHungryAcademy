8de1b5a07bcf87f7288848886d50bd28f8e2b679
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-09-05 07:42:15 -0400

    9/3/2024

---
+++ src/cs_and_programming/CS_DESIGN_PATTERNS/BuilderDesignPattern.java
@@ -0,0 +1,31 @@
+package cs_and_programming.CS_DESIGN_PATTERNS;
+
+public class BuilderDesignPattern {
+    /*
+    your cache needs to have multiple configuration options, like capacity, expiration time, or eviction strategy (LRU, FIFO). The builder pattern allows you to construct the cache with different configurations in a more readable and flexible way.
+     */
+    private int capacity = 100;
+    private EvictionStrategy strategy = EvictionStrategy.LRU;
+
+    public BuilderDesignPattern setCapacity(int capacity) {
+        this.capacity = capacity;
+        return this;
+    }
+
+    public BuilderDesignPattern setEvictionStrategy(EvictionStrategy strategy) {
+        this.strategy = strategy;
+        return this;
+    }
+
+    public SingletonDesignPattern build() {
+        SingletonDesignPattern cache = SingletonDesignPattern.getInstance(capacity);
+        // we can also set the eviction strategy in the SingletonDesignPattern instance here if needed
+        return cache;
+    }
+
+    private enum EvictionStrategy {
+        LRU,   // Least Recently Used
+        FIFO,  // First In First Out
+        LFU    // Least Frequently Used
+    }
+}
