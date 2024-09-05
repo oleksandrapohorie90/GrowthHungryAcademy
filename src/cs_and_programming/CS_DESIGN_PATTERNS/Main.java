504e22722078a63cdb67010a10571c53042ad712
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-09-05 08:05:03 -0400

    9/3/2024

--- src/cs_and_programming/CS_DESIGN_PATTERNS/Main.java
+++ src/cs_and_programming/CS_DESIGN_PATTERNS/Main.java
@@ -1,5 +1,7 @@
 package cs_and_programming.CS_DESIGN_PATTERNS;
 
+import static cs_and_programming.CS_DESIGN_PATTERNS.BuilderDesignPattern.EvictionStrategy.LRU;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Cache cache = new Cache(5); //cache capacity of 5
@@ -15,11 +17,11 @@ public class Main {
         System.out.println(cache.containsKey("Alex")); //false, since capacity was reached and Alex got removed
 
         System.out.println(cache.size()); //always will be 5
-        SingletonDesignPattern cache1 = SingletonDesignPattern.getInstance(100); // Creates the instance
-        SingletonDesignPattern cache2 = SingletonDesignPattern.getInstance(200); // Returns the existing instance
+        SingletonDesignPattern cache1 = SingletonDesignPattern.getInstance(100,LRU); // Creates the instance
+        SingletonDesignPattern cache2 = SingletonDesignPattern.getInstance(200,LRU); // Returns the existing instance
         System.out.println(cache1 == cache2); // true - both references point to the same instance
 
         FactoryDesignPattern cacheFactory = new LRUCacheFactory();
-        SingletonDesignPattern lruCache = cacheFactory.createCache(100);
+        SingletonDesignPattern lruCache = cacheFactory.createCache(100, LRU);
     }
 }
