88d672db3fbfbc563ecd74b10bb0807d05457f88
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-09-29 11:59:14 -0400

    9/29/2024

---
+++ src/java_intro/week2/school/management/CourseManager.java
@@ -0,0 +1,22 @@
+package java_intro.week2.school.management;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseManager {
+    private List<Student> students;
+
+    public CourseManager() {
+        this.students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student){
+        this.students.add(student);
+    }
+
+    public void displayAllStudents(){
+        for(Student student: students){
+            student.displayDetails();
+        }
+    }
+}
