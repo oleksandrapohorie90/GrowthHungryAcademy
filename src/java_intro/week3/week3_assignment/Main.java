937a8de8f4c3d02b0213763f7e424a2d51fc0079
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-10-14 20:45:11 -0400

    10/14/2024

--- src/java_intro/week3/week3_assignment/Main.java
+++ src/java_intro/week3/week3_assignment/Main.java
@@ -2,6 +2,8 @@ package java_intro.week3.week3_assignment;
 
 public class Main {
     public static void main(String[] args) {
+
+        Contact c1 = new Contact("Jacky","6573625656", "olga3@yahoo.com");
         ContactManagement cm = new ContactManagement();
         PhoneNumberLookUp pn = new PhoneNumberLookUp();
         UniqueEmailDomains ud = new UniqueEmailDomains();
@@ -11,11 +13,13 @@ public class Main {
         cm.addContact(new Contact("tom", "9876780909", "tom1@gmail.com"));
 
         cm.displayAllContacts();
+        pn.addContactToPhones(c1);
 
 
         pn.displayPhoneNumber("tom");
 
 
         ud.extractDomainNames();
+        ud.displayUniqueDomains();
     }
 }
