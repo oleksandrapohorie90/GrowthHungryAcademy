9fb110540ddc7279c5f4ae674e2a976952d31aee
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-12-26 21:48:27 -0500

    12/26/2024

---
+++ src/foundation_pre_course/week11_java/Convert1DArrInto2DArr.java
@@ -0,0 +1,19 @@
+package foundation_pre_course.week11_java;
+
+public class Convert1DArrInto2DArr {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if(original.length != m*n){
+            return new int[0][0];
+        }
+        int[][] arr = new int[m][n];
+        int current_index = 0;
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                arr[i][j] = original[current_index];
+                current_index++;
+            }
+        }
+        return arr;
+    }
+
+}
