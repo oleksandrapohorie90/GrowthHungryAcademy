30349559977ee3e86fc9c774f2fbb66b524d3b75
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-12-30 07:55:43 -0500

    12/30/2024

---
+++ src/java_intro/week10/practical_10/Employee.java
@@ -0,0 +1,35 @@
+package java_intro.week10.practical_10;
+
+class Employee {
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
