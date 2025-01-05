5d273eee805f01dd6d36240955657363a1b7aa0b
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-01-05 13:28:13 -0500

    1/5/2025

--- src/data_structures_and_algorithms_CS210/week17/FirstBadVersion.java
+++ src/data_structures_and_algorithms_CS210/week17/FirstBadVersion.java
@@ -7,7 +7,7 @@ public class FirstBadVersion extends VersionControl {
         return false;
     }
 
-    //VersionControl is another class that has isBadVersion FunctionalProgrammingAndLambda defined
+    //VersionControl is another class that has isBadVersion FunctionalProgrammingAndLambda_1 defined
     public int firstBadVersion(int n) {
         int left = 1;
         int right = n;
