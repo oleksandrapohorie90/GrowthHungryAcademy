8202ce8cd067cd7de2099f982f5ba386ab39863c
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-01-09 07:03:01 -0500

    1/7/2025

--- src/foundation_pre_course/week10/ValidAnagram.java
+++ src/foundation_pre_course/week10/ValidAnagram.java
@@ -9,6 +9,7 @@ public class ValidAnagram {
 
         System.out.println(isAnagram(str1, str2));
         System.out.println(isAnagram2(str1, str2));
+        System.out.println(isAnagram3(str1, str2));
     }
 
     public static boolean isAnagram(String str1, String str2) {
@@ -21,6 +22,28 @@ public class ValidAnagram {
         return Arrays.equals(arr1, arr2);
     }
 
+    /**
+ create a freq array w/ 26 indexes and iterate over str1, take each char and sub 'q' from it, we have 26 letters in english alphabet
+     if you take 'a' - 'a'(sub ascii codes here = 97-97) => 0
+     'b' - 'a' => 1 (98-97)
+     ...just count how many letters you have, freq will count how many letters you have freq0 = how many 1's I have, freq1 how many b's etc
+     'z' - 'a' => 25 (122-97)
+
+     after 2 loops there will be only 0's in freq, bc letters were the same and each letter was incremented and decremented same num of times, if anagram
+
+     if f is not 0 then for certain letter the freq were different
+     */
+    public static boolean isAnagram3(String str1, String str2) {
+        int[] frequency = new int[26];
+        for (int i =0; i< str1.length(); i++) frequency[str1.charAt(i) - 'a']++;
+        for (int i =0; i< str2.length(); i++) frequency[str2.charAt(i) - 'a']--;
+        for (int f : frequency) if (f != 0) return false;
+
+        return true;
+
+    }
+
+    //less performant bc we convert toCharArr
     public static boolean isAnagram2(String str1, String str2) {
         int[] frequency = new int[26];
         for (char c : str1.toCharArray()) frequency[c - 'a']++;
