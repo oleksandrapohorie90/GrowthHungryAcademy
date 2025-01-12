839f6c344c269fc68213f65142da6868ddb7138e
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-01-12 11:43:54 -0500

    1/10/2025

--- src/foundation_pre_course/week11/FirstUniqueCharacterInAString.java
+++ src/foundation_pre_course/week11/FirstUniqueCharacterInAString.java
@@ -13,9 +13,27 @@ Output: 0
 Explanation:
 
 The character 'l' at index 0 is the first character that does not occur at any other index.
+LEFTMOST IN THE STRING AND APPEARS ONLY ONCE
      */
+    //WAY FROM JAVA SESSION 1 FOR COUNT/FIND THE FREQUENCIES
     public int firstUniqChar(String s) {
 
+        //find the frequencies 1st
+        int[] freq = new int[26];
+
+        //count the frequencies
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            freq[c - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (freq[c - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
