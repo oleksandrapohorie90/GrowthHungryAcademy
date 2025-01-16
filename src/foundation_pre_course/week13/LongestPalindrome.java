97a05210bc7bcd2d8c97dd0f2a3146e44090d394
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-01-15 22:01:40 -0500

    1/14/2025

--- src/foundation_pre_course/week13/LongestPalindrome.java
+++ src/foundation_pre_course/week13/LongestPalindrome.java
@@ -27,18 +27,24 @@ Explanation: The longest palindrome that can be built is "a", whose length is 1.
         if (s == null || s.length() == 0) {
             return 0;
         }
-
         HashSet<Character> hash_set = new HashSet<Character>();
         int palindrome_length = 0;
         for (char ch : s.toCharArray()) {
             if (hash_set.contains(ch)) {
+                // Found a pair of characters `ch`,
+                // increase the palindrome_length by 2
+                // and remove `ch` from hash set.
                 hash_set.remove(ch);
                 palindrome_length += 2;
             } else {
+                // Character `ch` doesn't have a pair yet,
+                // so put it in the hash set for now.
                 hash_set.add(ch);
             }
-
         }
+        // If hash set is not empty, it means there are
+        // characters which we couldn't pair. We can use
+        // one of them – put in the center of our palindrome.
         if (!hash_set.isEmpty()) {
             palindrome_length++;
         }
