3a05afac255eb6a2aeff3367c70bb1687d1a363b
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-01-19 11:59:07 -0500

    1/14/2025

--- src/foundation_pre_course/week13/SpiralMatrix.java
+++ src/foundation_pre_course/week13/SpiralMatrix.java
@@ -15,44 +15,27 @@ public class SpiralMatrix {
     }
 
     public static List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res = new ArrayList<>();
+        List<Integer> res = new ArrayList<Integer>();
         if (matrix.length == 0 || matrix[0].length == 0) {
             return res;
         }
-        // Initialize boundaries
         int top = 0;
         int bottom = matrix.length - 1;
         int left = 0;
         int right = matrix[0].length - 1;
-
-        while (top <= bottom && left <= right) {
-            // Traverse from left to right along the top row
-            for (int i = left; i <= right; i++) {
-                res.add(matrix[top][i]);
-            }
-            top++;
-
-            // Traverse from top to bottom along the right column
-            for (int i = top; i <= bottom; i++) {
-                res.add(matrix[i][right]);
-            }
-            right--;
-
-            if (top <= bottom) {
-                // Traverse from right to left along the bottom row
-                for (int i = right; i >= left; i--) {
-                    res.add(matrix[bottom][i]);
-                }
-                bottom--;
-            }
-
-            if (left <= right) {
-                // Traverse from bottom to top along the left column
-                for (int i = bottom; i >= top; i--) {
-                    res.add(matrix[i][left]);
-                }
-                left++;
-            }
+        while (true) {
+            // 1. Iterate over top row, left to right.
+            for (int i = left; i <= right; i++) res.add(matrix[top][i]);
+            if (++top > bottom) break;
+            // 2. Iterate over right column, top to bottom.
+            for (int i = top; i <= bottom; i++) res.add(matrix[i][right]);
+            if (left > --right) break;
+            // 3. Iterate over bottom row, right to left.
+            for (int i = right; i >= left; i--) res.add(matrix[bottom][i]);
+            if (top > --bottom) break;
+            // 4. Iterate over left column, bottom to top.
+            for (int i = bottom; i >= top; i--) res.add(matrix[i][left]);
+            if (++left > right) break;
         }
         return res;
     }
