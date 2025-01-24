199baeac451b5c52e6f4b172c01abae313697946
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-01-23 21:44:18 -0500

    1/23/2025

--- src/data_structures_and_algorithms_CS210/Second_Cohort/week1_arrays/SortColors.java
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week1_arrays/SortColors.java
@@ -3,25 +3,49 @@ package data_structures_and_algorithms_CS210.Second_Cohort.week1_arrays;
 import java.util.Arrays;
 
 public class SortColors {
+
+    /**
+     * Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+     * <p>
+     * We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+     * <p>
+     * You must solve this problem without using the library's sort function.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: nums = [2,0,2,1,1,0]
+     * Output: [0,0,1,1,2,2]
+     * Example 2:
+     * <p>
+     * Input: nums = [2,0,1]
+     * Output: [0,1,2]
+     */
+    public static void main(String[] args) {
+        int[] arr = {2, 0, 2, 1, 1, 0};
+        sortColors(arr);
+    }
+
     //Dutch National flag algorithm: red, white, blue
     public static void sortColors(int[] nums) {
-        int zeroInsertPos = 0;
-        int twoInsertPos = nums.length - 1;
-        int i = 0;
 
-        while (i <= twoInsertPos) {
-            if (nums[i] == 0) {
-                swap(nums, zeroInsertPos, i);
-                zeroInsertPos++;
-                i++;
-            } else if (nums[i] == 1) {
-                i++;
-            } else {
-                swap(nums, i, twoInsertPos);
-                twoInsertPos--;
+        int insert_position = 0;
+        System.out.println("Input arr before sort: " + Arrays.toString(nums));
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 1) {
+                swap(nums, i, insert_position);
+                insert_position++;
             }
         }
-        System.out.println(Arrays.toString(nums));
+
+        for (int i = insert_position; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                swap(nums, i, insert_position);
+                insert_position++;
+            }
+        }
+
+        System.out.println("Input arr after sort: " + Arrays.toString(nums));
+
     }
 
     public static void swap(int[] nums, int i, int j) {
