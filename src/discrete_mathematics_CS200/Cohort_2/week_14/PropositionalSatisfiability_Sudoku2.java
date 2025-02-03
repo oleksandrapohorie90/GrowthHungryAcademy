b950b9b5dff834de4332f69382e64a370e5f0498
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-02-02 19:55:09 -0500

    2/2/2025

---
+++ src/discrete_mathematics_CS200/Cohort_2/week_14/PropositionalSatisfiability_Sudoku2.java
@@ -0,0 +1,42 @@
+package discrete_mathematics_CS200.Cohort_2.week_14;
+
+public class PropositionalSatisfiability_Sudoku2 {
+    //propositional equivalences - Ch. 1.3
+    int everyColumnContainsEveryNumber() {
+        int every_column_contains_every_number = 1;
+
+        //we 1st iterate over columns
+        for (int j = 1; j <= 9; j++) {
+            //for each column we verify that every num is in this column
+            int every_number_is_in_the_column = 1;
+
+            //we iterate over the numbers
+            for (int n = 1; n <= 9; n++) {
+
+//for each number we verify it is in this column
+                int number_n_is_in_the_column = 0; //because we are using OR operation
+
+                //now iterate over the columns
+                for (int i = 1; i <= 9; i++) {
+                    //using OR operation
+                    //if column i in row j contains number n,
+                    //then number_n_is_in_the_column will become 1 (true)
+
+                    //number_n_is_in_the_column |= p(i, j, n); //one of the columns contains this number n
+                }
+                //using AND operation
+                //if NOT number_n_is_in_the_row, then
+                //every_number_is_in_the_row will become 0 (false)
+                every_number_is_in_the_column &= number_n_is_in_the_column;//then we take AND for all of these values
+            }
+            //using AND operation
+            //if NOT very_number_is_in_the_row, then
+            //every_row_contains_every_number will become 0 (false)
+            every_column_contains_every_number &= every_number_is_in_the_column;//we take AND  for every column that every column has every number
+        }
+
+        return everyColumnContainsEveryNumber();
+    }
+}
+
+
---
+++ src/discrete_mathematics_CS200/Cohort_2/week_14/PropositionalSatisfiability_Sudoku3.java
@@ -0,0 +1,48 @@
+package discrete_mathematics_CS200.Cohort_2.week_14;
+
+public class PropositionalSatisfiability_Sudoku3 {
+    //final every 3 x 3 block contains every_number
+
+    int everyBlockContainsEveryNumber() {
+        int all_blocks_have_all_numbers = 1;
+
+        for (int r = 0; r <= 2; r++) {
+            for (int s = 0; s <= 2; s++) {
+                //(r=0, s=0) <-> the 1st block that will verify, i.e. grid[1..3][1..3]
+                //(r=0, s=1) <-> the 2nd block that will verify, i.e. grid[1..3][4..6]
+                //(r=0, s=2) <-> the 3rd block that will verify, i.e. grid[1..3][7..9]
+
+
+                //(r=1, s=0) <-> the 4th block that will verify, i.e. grid[4..6][1..3]
+                //(r=1, s=1) <-> the 5th block that will verify, i.e. grid[4..6][4..6]
+                //(r=1, s=2) <-> the sixth block that will verify, i.e. grid[4..6][7..9]
+
+
+                //(r=2, s=0) <-> the seventh block that will verify, i.e. grid[7..9][1..3]
+                //(r=2, s=1) <-> the eighth block that will verify, i.e. grid[7..9][4..6]
+                //(r=2, s=2) <-> the ninth block that will verify, i.e. grid[7..9][7..9]
+
+                int current_block_has_all_numbers = 1;
+
+                for (int n = 1; n <= 9; n++) {
+                    //verify that every number n=1..9 is present inside the current 3x3 block
+
+                    int number_n_is_present = 0;
+
+                    for (int i = 1; i <= 3; i++) {
+                        for (int j = 1; j <= 3; j++) {
+                            //iterating over the current (3r +i, 3s +j) 3x 3 block
+                            //if any of these values are true than number_n_is present is True
+                            //number_n_is_present |= p(3 * r + i, 3 * s + j, n);
+                        }
+                    }
+                    //then AND operation
+                    current_block_has_all_numbers &= number_n_is_present;
+                }
+                all_blocks_have_all_numbers &= current_block_has_all_numbers;
+            }
+        }
+
+        return all_blocks_have_all_numbers;
+    }
+}
