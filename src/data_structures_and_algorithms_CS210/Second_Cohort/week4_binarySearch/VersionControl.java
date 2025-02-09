19d0c59f8ab25270c64e2a9b36930ef06c9448f9
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-02-09 13:42:53 -0500

    2/4/2025

---
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week4_binarySearch/VersionControl.java
@@ -0,0 +1,8 @@
+package data_structures_and_algorithms_CS210.Second_Cohort.week4_binarySearch;
+
+public class VersionControl {
+   public boolean isBadVersion(int version){
+
+       return true;
+    }
+}
