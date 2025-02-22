47c32049f1f9c8eb01fd1ae2744b3004a4fbe5eb
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-02-22 18:46:56 -0500

    2/22/2025

---
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week5_twoPointers_slidingWindow/SumOfSquareNumbers.java
@@ -0,0 +1,6 @@
+package data_structures_and_algorithms_CS210.Second_Cohort.week5_twoPointers_slidingWindow;
+
+public class SumOfSquareNumbers {
+
+
+}
--- src/data_structures_and_algorithms_CS210/Second_Cohort/week5_twoPointers_slidingWindow/Three_Sum.java
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week5_twoPointers_slidingWindow/Three_Sum.java
@@ -1,5 +1,7 @@
 package data_structures_and_algorithms_CS210.Second_Cohort.week5_twoPointers_slidingWindow;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Three_Sum {
@@ -44,10 +46,51 @@ public class Three_Sum {
          *
          *
          */
+        List<List<Integer>> ans = new ArrayList<>();
 
+        // Sort the array
+        Arrays.sort(nums);
 
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate elements for i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    // Found a triplet with zero sum
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                    // Skip duplicate elements for j
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+
+                    // Skip duplicate elements for k
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+
+                    // Move the pointers
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    // Sum is less than zero, increment j to increase the sum
+                    j++;
+                } else {
+                    // Sum is greater than zero, decrement k to decrease the sum
+                    k--;
+                }
+            }
+        }
+        return ans;
     }
-
-
 }
+
 
