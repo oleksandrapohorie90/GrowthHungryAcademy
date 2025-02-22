5cdce0c7548275742248b56d883e618d57424107
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-02-22 18:21:07 -0500

    2/22/2025

--- src/data_structures_and_algorithms_CS210/Second_Cohort/week5_twoPointers_slidingWindow/Three_Sum.java
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week5_twoPointers_slidingWindow/Three_Sum.java
@@ -19,8 +19,9 @@ public class Three_Sum {
      * The distinct triplets are [-1,0,1] and [-1,-1,2].
      * Notice that the order of the output and the order of the triplets does not matter.
      */
-    //approach for TwoSum II - Input Array Is Sorted , fix i
+    //approach for TwoSum II - Input Array Is Sorted , fixed i number
     //to find a pair which sums up to TARGET in a sorted Array
+    //left to the beg of arr, right to the end of arr, if sum of 2 nums(from end and beg) is bigger than  target num - decrease the right pointer, if less than target - you increase left pointer
     //nums[j] + nums[k] + nums[i] = 0
     //nums[j] + nums[k] = -nums[i]
     //difference is that in this proble, there can be more than 1 pairs or 0 pairs, but in TwoSum the 1 solution guaranteed
@@ -30,7 +31,9 @@ public class Three_Sum {
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
-
+        /**
+         *
+         */
     }
 
 
