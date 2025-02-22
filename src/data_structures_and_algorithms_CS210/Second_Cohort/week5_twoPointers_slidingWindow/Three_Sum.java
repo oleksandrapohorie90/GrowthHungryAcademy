ea27d196dc8ac7542ca289104e175b2cb94b727b
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-02-22 17:52:12 -0500

    2/22/2025

--- src/data_structures_and_algorithms_CS210/Second_Cohort/week5_twoPointers_slidingWindow/Three_Sum.java
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week5_twoPointers_slidingWindow/Three_Sum.java
@@ -1,13 +1,15 @@
 package data_structures_and_algorithms_CS210.Second_Cohort.week5_twoPointers_slidingWindow;
 
+import java.util.List;
+
 public class Three_Sum {
     /**
      * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
-     *
+     * <p>
      * Notice that the solution set must not contain duplicate triplets.
-     *
+     * <p>
      * Example 1:
-     *
+     * <p>
      * Input: nums = [-1,0,1,2,-1,-4]
      * Output: [[-1,-1,2],[-1,0,1]]
      * Explanation:
@@ -17,7 +19,20 @@ public class Three_Sum {
      * The distinct triplets are [-1,0,1] and [-1,-1,2].
      * Notice that the order of the output and the order of the triplets does not matter.
      */
-    //approach for TwoSum, fix i
+    //approach for TwoSum II - Input Array Is Sorted , fix i
+    //to find a pair which sums up to TARGET in a sorted Array
     //nums[j] + nums[k] + nums[i] = 0
     //nums[j] + nums[k] = -nums[i]
+    //difference is that in this proble, there can be more than 1 pairs or 0 pairs, but in TwoSum the 1 solution guaranteed
+    //Brute-Force
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+    }
+
+
 }
+
