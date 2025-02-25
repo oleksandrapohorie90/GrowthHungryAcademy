97bc19224f718bf0b12f1332b142b9fb2d1f5517
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-02-25 07:09:38 -0500

    2/25/2025

--- src/data_structures_and_algorithms_CS210/Second_Cohort/week6_recurssion/FibonacciNumber.java
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week6_recurssion/FibonacciNumber.java
@@ -3,6 +3,8 @@ package data_structures_and_algorithms_CS210.Second_Cohort.week6_recurssion;
 public class FibonacciNumber {
 
     public static void main(String[] args) {
+        //Recursion is a property of a function when computing it, relies on computing itself
+
         /** Next fib num is the sum of previous 2 nums
          * The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
          *
@@ -18,13 +20,18 @@ public class FibonacciNumber {
          *
          */
         int n = 2;
+        //It is an exponential function, computing n-th fib number is proportional to n-th fib number, its computation time will be proportional to T(n) = O(F(n)) = O(2^n)
         //F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+        //in this proble the restriction is that n is <= 30, so it will be 2^30 = 1 billion operations, so it will timeout if we leave the first solution
+
         System.out.println(fib(n));
 
     }
 
     public static int fib(int n) {
         if (n <= 1) return n;
+        //you need to compute again fib function but for smaller parameters
         return fib(n - 1) + fib(n - 2);
     }
 
