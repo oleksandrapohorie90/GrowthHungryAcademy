a9aa1a14cb53b21290d8fe50592598300fff39ae
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-02-26 21:18:55 -0500

    2/25/2025

--- src/data_structures_and_algorithms_CS210/Second_Cohort/week6_recurssion/Permutations.java
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week6_recurssion/Permutations.java
@@ -4,38 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Permutations {
+
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
         backtrack(list, new ArrayList<>(), nums);
         return list;
     }
 
-    // nums = [1, 2, 3]
-
-    // Layer #0 (tempList):
-    // Layer #0 (iterator i):
-
-    // Layer #1 (tempList):
-    // Layer #1 (iterator i):
-
-    // Layer #2 (tempList):
-    // Layer #2 (iterator i):
-
-    // Layer #3 (tempList):   [2, 3, 1]
-    // Layer #3 (list): [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
-
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums) {
         if (tempList.size() == nums.length) {
             list.add(new ArrayList<>(tempList));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
             if (tempList.contains(nums[i])) {
-                continue; // element already exists, skip
+                continue;
             }
             tempList.add(nums[i]);
             backtrack(list, tempList, nums);
-            tempList.remove(tempList.size() - 1);
+            tempList.removeLast();
         }
     }
 }
