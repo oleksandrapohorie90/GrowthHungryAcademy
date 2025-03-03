e7854f55617b78763f0b5c4874b4ab308582f1ec
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-03-02 20:17:41 -0500

    3/2/2025

--- src/data_structures_and_algorithms_CS210/Second_Cohort/week6_recurssion/FibonacciNumber_Memoization.java
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week6_recurssion/FibonacciNumber_Memoization.java
@@ -2,6 +2,7 @@ package data_structures_and_algorithms_CS210.Second_Cohort.week6_recurssion;
 
 public class FibonacciNumber_Memoization {
     //memoization - when we save the result of calculating a certain fib number, so then later on when we need to calculate that exact same fib number again we can just look up that value that we already calculated instead of redoing all those calculations all over again
+    private static long[] fibonacciCache;
 
     public static void main(String[] args) {
         /**
@@ -10,14 +11,27 @@ public class FibonacciNumber_Memoization {
          *  +  +  +  +  +  +  +  +  +  +
          */
         int n2 = 6; //<= 8, the 6th fib number is the 5th fib number + 4th fib number
-        //f(n) = f(n-1) + f(n-2)
-        fibonacci(n2);
+
+        fibonacciCache = new long[n2 + 1]; //because 1st fib num in the sequence is 0 => 0 1 1 2
+
+        for (int i = 0; i <= n2; i++) {
+            //f(n) = f(n-1) + f(n-2)
+            System.out.print(fibonacci(i) + " ");
+        }
+
     }
-    private static long fibonacci(int n2){
+
+    private static long fibonacci(int n2) {
         //base case - a case where the condition is met and it stops/prevents recursive call, each recursive algorithms needs a base case to prevent it from recursing infinitely
-        if(n2<=1){
+        if (n2 <= 1) {
             return n2;
         }
-        return (fibonacci(n2-1)+fibonacci(n2-2));
+        if (fibonacciCache[n2] != 0) {
+            return fibonacciCache[n2];
+        }
+
+        long nthFibonacciNumber = (fibonacci(n2 - 1) + fibonacci(n2 - 2));
+        fibonacciCache[n2] = nthFibonacciNumber;
+        return nthFibonacciNumber;
     }
 }
