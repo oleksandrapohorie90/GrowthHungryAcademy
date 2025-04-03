d7c14894c3131935be60bc6c2a68607db41fb44c
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-04-03 18:24:06 -0400

    3/19/2025

--- src/cs_and_programming/Second_Cohort/Lexer/P_I/Lexer.java
+++ src/cs_and_programming/Second_Cohort/Lexer/P_I/Lexer.java
@@ -1,36 +1,146 @@
 package cs_and_programming.Second_Cohort.Lexer.P_I;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import cs_and_programming.Lexerexception;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+//raw input -> broken down into list of tokens
+//we want to support basic operations x= 3 + 5 *6;
+//we want to support some if (x < 0)
 
 public class Lexer {
-    public static List<String> tokenize(String input) {
-        List<String> tokens = new ArrayList<>();
+    //    public static List<String> tokenize(String input) {
+//        List<String> tokens = new ArrayList<>();
+//
+//        //define token patterns in order
+//        String[] tokenPatterns = {
+//                "\\bif\\b",           //if
+//                "\\belse\\b",         //else
+//                "\\bprint\\b",        //print
+//                "[a-zA-Z_][a-zA-Z0-9_]*",// identifiers (x, y, variable names)
+//                "\\d+",               //numbers
+//                "==|>=|<=|!=|>|<",    //comparison operators
+//                "[+\\-*/=]",          //math and assignment
+//                "[(){};]"             //punctuation
+//        };
+//
+//        //combine them into one big regex
+//        String combinedPattern = String.join("|", tokenPatterns);
+//
+//        //matcher to find all tokens
+//        Matcher matcher = Pattern.compile(combinedPattern).matcher(input);
+//
+//        while (matcher.find()) {
+//            tokens.add(matcher.group());
+//        }
+//
+//        return tokens;
+//    }
+    private static final char EOF_CHAR = '\0';
+    private static final Set<String> KEYWORDS = new HashSet<>(Arrays.asList("if"));
+    private final String input;
+    private int position;
+    private char currentChar;
 
-        //define token patterns in order
-        String[] tokenPatterns = {
-                "\\bif\\b",           //if
-                "\\belse\\b",         //else
-                "\\bprint\\b",        //print
-                "[a-zA-Z_][a-zA-Z0-9_]*",// identifiers (x, y, variable names)
-                "\\d+",               //numbers
-                "==|>=|<=|!=|>|<",    //comparison operators
-                "[+\\-*/=]",          //math and assignment
-                "[(){};]"             //punctuation
-        };
+    public Lexer(String input) {
+        this.input = input;
+        this.position = 0;
+        this.currentChar = input == null || input.isEmpty() ? EOF_CHAR : input.charAt(position);
+    }
 
-        //combine them into one big regex
-        String combinedPattern = String.join("|", tokenPatterns);
+    public Token nextToken() {
+        while (currentChar != EOF_CHAR) {
+            if (Character.isWhitespace(currentChar)) {
+                skipWhiteSpace();
+                continue;
+            }
+            if (Character.isDigit(currentChar)) {
+                return readNumber();
+            }
+            if (Character.isLetter(currentChar)) {
+                return readIdentifier();
+            }
+            switch (currentChar) {
+                case '=':
+                    advance();
+                    return new Token(TokenType.ASSIGNMENT, "=");
+                case '+':
+                    advance();
+                    return new Token(TokenType.PLUS, "+");
+                case '-':
+                    advance();
+                    return new Token(TokenType.MINUS, "-");
+                case '*':
+                    advance();
+                    return new Token(TokenType.MULTIPLY, "*");
+                case ';':
+                    advance();
+                    return new Token(TokenType.SEPARATOR, ";");
+                //same thing for minus, delete, multiply, brackets, (,{,[
+                default:
+                    throw new Lexerexception("Unsupported character: " + currentChar);
 
-        //matcher to find all tokens
-        Matcher matcher = Pattern.compile(combinedPattern).matcher(input);
+            }
+        }
+        return new Token(TokenType.EOF, "");
+    }
 
-        while (matcher.find()) {
-            tokens.add(matcher.group());
+    private Token readIdentifier() {
+        StringBuilder builder = new StringBuilder();
+        while (Character.isLetterOrDigit(currentChar)) {
+            builder.append(currentChar);
+            advance();//to be able to move to another character
+        }
+        String value = builder.toString();
+        return KEYWORDS.contains(value) ?
+                new Token(TokenType.IF, value) :
+                new Token(TokenType.IDENTIFIER, value);
+    }
+
+    private Token readNumber() {
+        StringBuilder builder = new StringBuilder();
+        while (Character.isDigit(currentChar)) {
+            builder.append(currentChar);
+            advance();//to be able to move to another character
+        }
+        return new Token(TokenType.NUMBER, builder.toString());
+    }
+
+    private void skipWhiteSpace() {
+        while (Character.isWhitespace(currentChar)) {
+            advance();
+        }
+    }
+
+    private void advance() {
+        position++;
+        currentChar = position < input.length() ? input.charAt(position)
+                : EOF_CHAR;
+    }
+
+    public static class Token {
+        public final TokenType tokenType;
+        public final String value;
+
+        public Token(TokenType tokenType, String value) {
+            this.tokenType = tokenType;
+            this.value = value;
         }
 
-        return tokens;
+        @Override
+        public String toString() {
+            return "Token{" +
+                    "tokenType=" + tokenType +
+                    ", value='" + value + '\'' +
+                    '}';
+        }
     }
+
+
+    public enum TokenType {
+        NUMBER, IDENTIFIER, PLUS, EOF, ASSIGNMENT, SEPARATOR, MINUS, IF, MULTIPLY
+    }
+
 }
--- src/cs_and_programming/Second_Cohort/Lexer/P_I/Main.java
+++ src/cs_and_programming/Second_Cohort/Lexer/P_I/Main.java
@@ -4,17 +4,26 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String code = """
-            x = 5;
-            if (x > 3) {
-                y = x + 2;
-            } else {
-                y = x * (2 + 3);
-            }
-            print y;
-        """;
 
-        List<String> tokens = Lexer.tokenize(code);
-        System.out.println(tokens);
+        String rawInput = "xyz = 5465 +6 \n m = 5 * 7";
+        Lexer lexer = new Lexer(rawInput);
+
+        Lexer.Token token;
+        do {
+            token = lexer.nextToken();
+            System.out.println(token);
+        } while (token.tokenType != Lexer.TokenType.EOF);
+//        String code = """
+//            x = 5;
+//            if (x > 3) {
+//                y = x + 2;
+//            } else {
+//                y = x * (2 + 3);
+//            }
+//            print y;
+//        """;
+
+        //List<String> tokens = Lexer.tokenize(code);
+        //System.out.println(tokens);
     }
 }
