51e556002e505ecfbab5ed710d3283b118716d22
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-04-25 21:23:54 -0400

    3/19/2025

---
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/Neetcode/TwoSums_HashMap.java
@@ -0,0 +1,18 @@
+package data_structures_and_algorithms_CS210.Second_Cohort.Neetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSums_HashMap {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {}; // Dummy return, won't actually reach here
+    }
+}
