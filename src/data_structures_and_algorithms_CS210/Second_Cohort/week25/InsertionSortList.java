f7e756cea88c95712a5bcebbb3fa6641f2a4cdea
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-04-27 16:34:24 -0400

    3/19/2025

---
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week25/InsertionSortList.java
@@ -0,0 +1,36 @@
+package data_structures_and_algorithms_CS210.Second_Cohort.week25;
+
+public class InsertionSortList {
+    /**
+     * Definition for singly-linked list.
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode insertionSortList(ListNode head) {
+        for (ListNode cur = head; cur != null; cur = cur.next) {
+            for (ListNode j = head; j != cur; j = j.next) {
+                if (j.val > cur.val) {
+                    int t = j.val;
+                    j.val = cur.val;
+                    cur.val = t;
+                }
+            }
+        }
+        return head;
+    }
+}
