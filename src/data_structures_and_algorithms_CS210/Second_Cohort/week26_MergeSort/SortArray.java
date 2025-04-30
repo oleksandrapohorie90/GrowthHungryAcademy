ece714f09716a9a88cad497a5d371b2579fd39f6
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-04-30 08:22:51 -0400

    3/19/2025

--- src/data_structures_and_algorithms_CS210/Second_Cohort/week26_MergeSort/SortArray.java
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week26_MergeSort/SortArray.java
@@ -31,12 +31,36 @@ public class SortArray {
         //j for the right half (arr[mid+1..right])
         //
         //k for the position in the temporary array
+
+        //arr[2], arr[3], arr[4], arr[5] -> how many elements: right - left + 1 = 5 - 2 + 1 = 4(bc zero indexed)
+        //It's because you're counting how many total elements are between two indexes inclusive.
+        //“How many elements from index left to index right, including both ends?”
+        //Count of days from March 3 to March 6 = 6 - 3 + 1 = 4 days
         int len = right - left + 1;
         int[] tmp = new int[len];
 
         int i = left;       // pointer for left half
         int j = mid + 1;    // pointer for right half
         int k = 0;          // pointer for tmp[]
+
+        //Compare arr[i] and arr[j]
+        //
+        //Copy the smaller one into tmp[k]
+        //
+        //Move the corresponding pointer (i or j)
+        //
+        //Always advance k
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                tmp[k] = arr[i];
+                i++;
+            } else {
+                tmp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
 
     }
 }
