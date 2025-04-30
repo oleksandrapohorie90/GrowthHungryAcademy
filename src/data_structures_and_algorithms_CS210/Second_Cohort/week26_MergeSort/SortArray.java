d3d0795f5b1619f76267833f5537f451691543fb
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-04-30 08:26:15 -0400

    3/19/2025

--- src/data_structures_and_algorithms_CS210/Second_Cohort/week26_MergeSort/SortArray.java
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week26_MergeSort/SortArray.java
@@ -61,6 +61,53 @@ public class SortArray {
             k++;
         }
 
+        // Copy remaining elements from the left half
+        while (i <= mid) {
+            tmp[k] = arr[i];
+            i++;
+            k++;
+        }
+
+// Copy remaining elements from the right half
+        while (j <= right) {
+            tmp[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for (int x = 0; x < tmp.length; x++) {
+            arr[left + x] = tmp[x];
+        }
+
 
     }
 }
+
+//Azret's solution
+//public int[] sortArray(int[] nums) {
+//        MergeSort(nums, 0, nums.length - 1);
+//        return nums;
+//    }
+//
+//    public static void MergeSort(int[] arr, int left, int right) {
+//        if (left == right) return;
+//        int mid = (left + right) / 2;
+//        MergeSort(arr, left, mid);
+//        MergeSort(arr, mid + 1, right);
+//        Merge(arr, left, right);
+//    }
+//
+//    public static void Merge(int[] arr, int left, int right) {
+//        int len = right - left + 1;
+//        int[] tmp = new int[len];
+//        int mid = (left + right) / 2;
+//        for (int k = 0, i = left, j = mid + 1; k < len; k++) {
+//            if (i > mid) tmp[k] = arr[j++];
+//            else if (j > right) tmp[k] = arr[i++];
+//            else if (arr[i] < arr[j]) tmp[k] = arr[i++];
+//            else tmp[k] = arr[j++];
+//        }
+//        for (int k = 0, i = left; k < len; k++, i++) {
+//            arr[i] = tmp[k];
+//        }
+//    }
