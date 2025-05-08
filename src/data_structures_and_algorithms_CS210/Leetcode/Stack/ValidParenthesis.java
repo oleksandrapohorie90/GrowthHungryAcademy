a0b1ea227398a82bed56dd38f3c41d1746bf3f78
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-05-07 22:19:03 -0400

    5/1/2025

--- src/data_structures_and_algorithms_CS210/Leetcode/Stack/ValidParenthesis.java
+++ src/data_structures_and_algorithms_CS210/Leetcode/Stack/ValidParenthesis.java
@@ -3,34 +3,33 @@ package data_structures_and_algorithms_CS210.Leetcode.Stack;
 import java.util.Stack;
 
 public class ValidParenthesis {
-    public boolean isValidParenthesis(String s) {
+    public boolean isValid(String s) {
+        //(])
+
+        //st
+        //(
+
+        //create an empty stack
         Stack<Character> stack = new Stack<>();
 
         //loop through every char in the string
         for (char c : s.toCharArray()) {
-            //if char is an opening bracket, push(add) it onto the stack
-            if (c == '(' || c == '[' || c == '{') {
+            if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
-            } else {//if the char is a closing bracket
-                //if the stack is empty, there is no matching opening bracket, so return false
+            } else {
                 if (stack.isEmpty()) {
-                    return false;
+                    return false; //in case ')' comes first, dont check anymore
+                } else {
+                    char prev = stack.peek(); //(
+                    if ((prev == '(' && c == ')') || (prev == '{' && c == '}') || (prev == '[' && c == ']')) {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
                 }
-
-                //otherwise, get the top of the stack and check if it's the matching opening bracket
-                char top = stack.peek();
-                if ((c == ')' && top == '(') || (c == ']' && top == '[') || (c == '}' && top == '{')) {
-                    //if it is, pop the opening bracket from the stack
-                    stack.pop();
-                } else {//otherwise, the brackets dont match, so return false
-                    return false;
-                }
-
-
             }
         }
-        //if the stack is empty, all opening brackets have been closed, so return true
-        //Otherwise, there are unmatched opening brackets, so return false
+
         return stack.isEmpty();
     }
 }
