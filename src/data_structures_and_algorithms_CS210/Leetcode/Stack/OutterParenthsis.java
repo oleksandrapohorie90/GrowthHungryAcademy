a906bc1744e19862a64777e373b8adf18fbd3616
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-05-07 22:01:20 -0400

    5/1/2025

---
+++ src/data_structures_and_algorithms_CS210/Leetcode/Stack/OutterParenthsis.java
@@ -0,0 +1,25 @@
+package data_structures_and_algorithms_CS210.Leetcode.Stack;
+
+import java.util.Stack;
+
+public class OutterParenthsis {
+    public String removeOuterParentheses(String s) {
+        StringBuilder result = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (!stack.isEmpty()) {
+                    result.append(c);
+                }
+                stack.push(c);
+            } else if (c == ')') {
+                stack.pop();
+                if (!stack.isEmpty()) {
+                    result.append(c);
+                }
+            }
+        }
+        return result.toString();
+    }
+}
