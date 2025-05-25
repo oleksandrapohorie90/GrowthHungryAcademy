a6cf1386671a917d458deba2d3599c6dbc81c239
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-05-25 14:43:40 -0400

    5/19/2025

--- src/data_structures_and_algorithms_CS210/Second_Cohort/week23_24_Queue_Deque_Heap/KthLargest.java
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week23_24_Queue_Deque_Heap/KthLargest.java
@@ -1,8 +1,6 @@
 package data_structures_and_algorithms_CS210.Second_Cohort.week23_24_Queue_Deque_Heap;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class KthLargest {
 
@@ -10,39 +8,60 @@ public class KthLargest {
      * poll() deletes max or min element depending on min_heapify() or max_heapify()
      * k=3
      * [2,4,5,8] - 3rd largest = 4
-     *
+     * <p>
      * [2 3 4 5 8] =4
      * [2 3 4 5 5 8] = 5
-     *
+     * <p>
      * create 2 heaps:
      * min-heap topK = [4 5 8] to find kth largest look for min in topK
      * max-heap rest = [2]
-     *
+     * <p>
      * We add 4 so where to add this 4 ?
      * [2 4 5 8]
      * topK - [4 5 8]
      * rest - [2]
      * we add(6) > add to topK, then get min value and compare against the val
      * if(val >= topK.peek()){
-     *     then it belongs to topK
-     *     move that smaller val to others next to [2]
-     *     rest.offer(topK.poll());
-     *     move new val to topK
-     *     topK.offer(val);
-     *
-     *     NOW: topK - [5 6 8]
-     *          rest - [2 4]
-     *
-     *     But what if we add 3 its not bigger or equal
-     *     add(val=3)
-     *     else{
-     *         rest.offer(val);
-     *         OR
-     *         do nothing, its never used
-     *     }
+     * then it belongs to topK
+     * move that smaller val to others next to [2]
+     * rest.offer(topK.poll());
+     * move new val to topK
+     * topK.offer(val);
+     * <p>
+     * NOW: topK - [5 6 8]
+     * rest - [2 4]
+     * <p>
+     * But what if we add 3 its not bigger or equal
+     * add(val=3)
+     * else{
+     * rest.offer(val);
+     * OR
+     * do nothing, its never used
+     * }
      * }
      */
+    PriorityQueue<Integer> topK;
 
+    public KthLargest(int k, int[] nums) {
+        topK = new PriorityQueue<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = n - 1; i >= Math.max(n - k, 0); i--) {
+            topK.offer(nums[i]);
+        }
+    }
+
+    public int add(int val) {
+        if(topK.isEmpty()){
+            topK.offer(val);
+        } else if (val >= topK.peek()) {
+            topK.poll();
+            topK.offer(val);
+        }
+        //for else do nothing
+        return topK.peek();
+    }
 
 //    private int k;
 //    private List<Integer> scores;
@@ -88,5 +107,5 @@ public class KthLargest {
 //            // Otherwise, we have k or more elements, so we can return the k-th largest.
 //            return this.scores.get(scores.size() - k);
 //        }
-    }
+}
 }
