13fc527ea44fab49d6bad15cebee58820dfc8d43
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-05-30 20:12:06 -0400

    5/19/2025

---
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week28_Greedy_Algorithms/ValidParenthesisString.java
@@ -0,0 +1,32 @@
+package data_structures_and_algorithms_CS210.Second_Cohort.week28_Greedy_Algorithms;
+
+import java.util.Stack;
+
+public class ValidParenthesisString {
+
+    public boolean checkValidString(String s) {
+        //what if asterisks appear before the closing bracket
+        Stack<Integer> left_indices = new Stack<>();
+        Stack<Integer> asterisk_indices = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                left_indices.push(i);
+            } else if (c == '*') {
+                asterisk_indices.push(i);
+            } else {
+                if (!left_indices.isEmpty()) {
+                    left_indices.pop(); //means there is smth we can match it with
+                } else if (!asterisk_indices.isEmpty()) {
+                    asterisk_indices.pop();
+                }else{
+                    return false; //this closing bracket cant be matched
+                }
+            }
+        }
+        while (!left_indices.isEmpty()){
+            //can we match the left indices ?
+        }
+    }
+}
