af8f402c499e2b32d5014c8d4651f2830332941e
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-06-08 15:35:30 -0400

    5/19/2025

--- src/data_structures_and_algorithms_CS210/Second_Cohort/week28_Greedy_Algorithms/NonOverlappingIntervals.java
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week28_Greedy_Algorithms/NonOverlappingIntervals.java
@@ -5,6 +5,65 @@ import java.util.PriorityQueue;
 
 public class NonOverlappingIntervals {
     public int eraseOverlapIntervals(int[][] intervals) {
+        // [[1,2],[2,3],[3,4],[1,3], [3,4], [1,2]]
+        // [[1,2], [2,3],[3,4]]
+        // minheap
+        // top - 2
+
+        // start 1 >=?  minheap 2  good
+        // minheap.pop
+        // add current end 2 add to minheap
+        // start 1 >=?  minheap 2  not good
+        // count of return++
+        // minheap.pop
+        // add current end 3 add to minheap
+        // start 2 >=?  minheap 3   not good
+        // count of return++
+        // minheap.pop
+        // add current end 3 add to minheap
+        // start 3 >=?  minheap 3 good
+        // minheap.pop
+        // add current end 4 add to minheap
+        // start 3 >=?  minheap 4 not good
+        // count of return++
+
+        // ignore
+        // [[1,2],[1,3], [2,3],[3,4],]
+
+        //count++
+
+        // [[1,2],[2, 3]]
+
+        // [,[1,11],[1,100], [2,12],[11,22]]
+        // minheap 11 current 1
+        // count++
+        // minheap 11 current 2
+        // count++
+        // minheap 11 current 11
+        //
+
+        // [[-52,31],[-73,-26],[82,97],[-65,-11],[-62,-49],[95,99],[58,95],[-31,49],[66,98],[-63,2],[30,47],[-40,-26]]
+
+
+        // [-73, -26][-65, -11] [-63, 2][-62, -49][-52, 31][-40, -26][-31, 49][30, 47][58, 95][66, 98][82, 97][95, 99]
+
+        // -26 ? -65 not good
+        // count++
+
+        // -26 ? -63 not good count++
+
+        // -26 ? -62 not good count ++
+
+        // -26 ? -52 not good count++
+        // -26 ? -40 not good count++
+        // -26 ? -31 not good count++
+        // -26 ? 30 add minheap 47
+        // 47 ? 58 add minheap 95
+        // 95 ? 66 not good count++
+        // 95 ? 82 not good count++
+        // 95 ? 95
+
+
         //sort the array first
         // [[1,2],[2,3],[3,4],[1,3]]
         //Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
