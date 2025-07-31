02342047b6b1f332e3ccbe0c56011d29cc3b8cef
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-07-30 21:24:54 -0400

    7/20/2025

---
+++ src/data_structures_and_algorithms_CS210/Leetcode/HashMap/ContainsDuplicates_II.java
@@ -0,0 +1,19 @@
+package data_structures_and_algorithms_CS210.Leetcode.HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicates_II {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        //correct approach will be HashMap
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
