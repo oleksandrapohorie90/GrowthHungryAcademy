fa88d0ba2cdfe7e85018a09ed3184dbae4c5ff55
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-08-03 12:49:40 -0400

    8/3/2025

--- src/cs_and_programming/RECAP_INTERPRETOR/Game.java
+++ src/cs_and_programming/RECAP_INTERPRETOR/Game.java
@@ -2,6 +2,9 @@ package cs_and_programming.RECAP_INTERPRETOR;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Game {
@@ -14,8 +17,19 @@ public class Game {
         //Scanner is better and simpler, in java we understand reading from disk is done through Buffer, and it reads by chunks
         File file = new File(playerProperties);
         Scanner input = new Scanner(file);
+
+        HashMap<String, String> map = new HashMap<>();
         while (input.hasNext()) {
             String line = input.nextLine();
+            String [] pair = line.split("="); //we have 2 elements
+            map.put(pair[0],pair[1]);
         }
+
+       //iterate through entries, map doesn't have indexes, only iterate through entries
+        //Map.Entry - inside class in Map
+        for(Map.Entry<String,String> entry: map.entrySet()){
+            System.out.println("Key = " + entry.getKey() + " Value = " + entry.getValue());
+        }
+
     }
 }
