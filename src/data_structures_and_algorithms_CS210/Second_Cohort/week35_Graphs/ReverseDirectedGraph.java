e901ffce65f66df430952b93265946f7e5d3f1f1
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-08-08 08:37:54 -0400

    8/7/2025

--- src/data_structures_and_algorithms_CS210/Second_Cohort/week35_Graphs/ReverseDirectedGraph.java
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week35_Graphs/ReverseDirectedGraph.java
@@ -2,6 +2,7 @@ package data_structures_and_algorithms_CS210.Second_Cohort.week35_Graphs;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -32,10 +33,29 @@ public class ReverseDirectedGraph {
 
         //create adjacency list for the reversed graph
         List<List<Integer>> reversedAdjList = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i <= n; i++) {
             reversedAdjList.add(new ArrayList<>());
 
         }
 
+        //construct the reversed graph
+        for (int u = 1; u <= n; u++) {
+            for (int v : originalAdjList.get(u)) {
+                //if there is an edge from u to v in the original graph, add a reversed edge from v to u in the reversed graph
+                reversedAdjList.get(v).add(u);
+            }
+        }
+
+        out.println(n);//numb of vertices
+        for (int i = 0; i <= n; i++) {
+            List<Integer> neighbors = reversedAdjList.get(i);
+            Collections.sort(neighbors); //sort neighbours in ascending order
+
+            for (int j = 0; j < neighbors.size(); j++) {
+                out.print(neighbors.get(j) + (j == neighbors.size() - 1 ? "" : " "));//print neighbor w/ space
+            }
+            out.println(); //new line after each vertex's adjacency list
+        }
+        out.flush();//flush the output buffer and close the printWriter
     }
 }
