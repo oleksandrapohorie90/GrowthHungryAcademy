5170a600e686cb4ce65f090623fa9ce59274d38d
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-08-08 08:39:37 -0400

    8/7/2025

--- src/data_structures_and_algorithms_CS210/Second_Cohort/week35_Graphs/ReverseDirectedGraph.java
+++ src/data_structures_and_algorithms_CS210/Second_Cohort/week35_Graphs/ReverseDirectedGraph.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ReverseDirectedGraph {
+    //usually we use adjacency list since we deal with large graph, not small so no adjacency matrix used too often
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
