64c438312e79721eddcbbcfe767fd499c8018572
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-08-31 14:09:47 -0400

    8/31/2025

--- src/cs_and_programming/Recap_SimpleDSL/Main.java
+++ src/cs_and_programming/Recap_SimpleDSL/Main.java
@@ -3,7 +3,17 @@ package cs_and_programming.Recap_SimpleDSL;
 import java.util.List;
 
 public class Main {
+
+    /*
+    PROGRAM   := STATEMENT+
+    STATEMENT := ASSIGNMENT | EXPRESSION
+    COMMAND   := LET NUMBER NUMBER
+    EXPRESSION:=
+    OPERATION := "ADD" | "SUB" | "DIV" | "MUL"
+    NUMBER    := [0-9]+
+     */
     public static void main(String[] args) {
+
         Lexer lexer = new Lexer("ADD 5 3 SUB 7 6  DIV 10 0");
         List<Token> tokens = lexer.tokenize();
 
