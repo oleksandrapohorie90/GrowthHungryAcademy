6dc16ffee142282824cfa4c3864d74811dd6165e
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-11-07 22:35:01 -0500

    11/7/2025

---
+++ src/data_structures_and_algorithms_CS210/Leetcode/SDET_Critical/FizzBuzz.java
@@ -0,0 +1,37 @@
+package data_structures_and_algorithms_CS210.Leetcode.SDET_Critical;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    //leetcode
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+        return result;
+    }
+}
